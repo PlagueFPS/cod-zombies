9d54b370953d37062af4da6f6e6614c2f41c8c9b
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-07-03 07:34:34 -0400

    map grid & cards structure complete

--- contentful/Types/contentful-types.ts
+++ contentful/Types/contentful-types.ts
@@ -1,1 +1,22 @@
 import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+export interface TypeFeaturedMapsFields {
+    title: EntryFieldTypes.Symbol;
+    date: EntryFieldTypes.Date;
+    slug: EntryFieldTypes.Symbol;
+    gameCategory: EntryFieldTypes.EntryLink<TypeGameCategorySkeleton>;
+    image: EntryFieldTypes.AssetLink;
+    description: EntryFieldTypes.Text;
+    body: EntryFieldTypes.RichText;
+}
+
+export type TypeFeaturedMapsSkeleton = EntrySkeletonType<TypeFeaturedMapsFields, "featuredMaps">;
+export type TypeFeaturedMaps<Modifiers extends ChainModifiers, Locales extends LocaleCode> = Entry<TypeFeaturedMapsSkeleton, Modifiers, Locales>;
+
+export interface TypeGameCategoryFields {
+    title: EntryFieldTypes.Symbol;
+    slug: EntryFieldTypes.Symbol;
+    description: EntryFieldTypes.Symbol;
+}
+
+export type TypeGameCategorySkeleton = EntrySkeletonType<TypeGameCategoryFields, "gameCategory">;
+export type TypeGameCategory<Modifiers extends ChainModifiers, Locales extends LocaleCode> = Entry<TypeGameCategorySkeleton, Modifiers, Locales>;
--- utils/contentful-utils.ts
+++ utils/contentful-utils.ts
@@ -1,5 +1,6 @@
-import type { Asset, EntriesQueries, EntrySkeletonType, UnresolvedLink } from "contentful";
+import type { Asset, EntriesQueries, Entry, EntrySkeletonType, UnresolvedLink } from "contentful";
 import { client } from "@/contentful/contentful"
+import { TypeGameCategorySkeleton } from "@/contentful/Types/contentful-types";
 
 export const getPosts = async <T extends EntrySkeletonType>(searchParams: EntriesQueries<T, undefined>) => {
   const response = await client.getEntries<T>(searchParams)
@@ -8,4 +9,8 @@ export const getPosts = async <T extends EntrySkeletonType>(searchParams: Entrie
 
 export const resolveAsset = (asset: UnresolvedLink<"Asset"> | Asset<undefined, string>) => {
   if ('fields' in asset && asset.fields.file) return asset
+}
+
+export const resolveEntry = (entry: UnresolvedLink<"Entry"> | Entry<TypeGameCategorySkeleton, undefined, string>) => {
+  if ('fields' in entry && entry.fields) return entry
 }
