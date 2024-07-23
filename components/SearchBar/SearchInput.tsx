9d891573501d0540e5b83588b15885e1532b80df
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-07-23 00:08:10 -0400

    Improved navigation UX

--- components/SearchBar/SearchInput.tsx
+++ components/SearchBar/SearchInput.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { GameCategory } from "@/types/GameCategory";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "../ui/command";
 import { Button } from "../ui/button";
@@ -21,6 +22,7 @@ interface SearchInputProps {
 }
 
 export default function SearchInput({ maps, gameCategories }: SearchInputProps) {
+  const router = useRouter()
   const [open, setOpen] = useState(false)
 
   useEffect(() => {
@@ -35,9 +37,14 @@ export default function SearchInput({ maps, gameCategories }: SearchInputProps) 
     return () => document.removeEventListener("keydown", down)
   }, [])
 
+  const onSelectHandler = (categorySlug: string | undefined, mapSlug: string) => {
+    setOpen(false)
+    router.push(`/${categorySlug}/${mapSlug}`)
+  }
+
   return (
     <>
-      <Button variant="outline" size="icon" className="sm:hidden mr-2" onClick={ () => setOpen(!open) } title="open search menu">
+      <Button variant="outline" size="icon" className="sm:hidden mr-2 bg-transparent" onClick={ () => setOpen(!open) } title="open search menu">
         <Search className="h-5 w-5" />
       </Button>
       <Button variant="outline" className="hidden sm:flex gap-8 text-foreground/70" onClick={ () => setOpen(!open) }>
@@ -53,8 +60,8 @@ export default function SearchInput({ maps, gameCategories }: SearchInputProps) 
           { gameCategories.map(game => (
             <CommandGroup heading={ game.title } key={ game.slug }>
               { maps.filter(map => map.category.slug === game.slug).map(map => (
-                <Link key={ `${game.slug}_${map.slug}` } href={ `/${map.category.slug}/${map.slug}` } onClick={ () => setOpen(!open) }>
-                  <CommandItem>
+                <Link key={ `${game.slug}_${map.slug}` } href={ `/${map.category.slug}/${map.slug}` } onClick={ () => setOpen(false) }>
+                  <CommandItem onSelect={ () => onSelectHandler(map.category.slug, map.slug) }>
                     <span className="blur-none">{ map.title }</span>
                   </CommandItem>
                 </Link>
