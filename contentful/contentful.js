c79d8761dda1c9eabf20c084851caf58535ecdbd
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-08-01 13:06:00 -0400

    Fix build error

--- contentful/contentful.js
+++ contentful/contentful.js
@@ -3,6 +3,6 @@ import { createClient } from 'contentful'
 export const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   environment: process.env.CONTENTFUL_ENVIRONMENT,
-  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
   host: process.env.CONTENTFUL_PREVIEW_HOSTNAME
 })
