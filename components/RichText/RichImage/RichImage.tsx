61efb080ca6de5d2893077ee7e11e02150b32b40
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-08-10 17:37:46 -0400

    disabled `AVIF` image formats & fixed image loader positioning

--- components/RichText/RichImage/RichImage.tsx
+++ components/RichText/RichImage/RichImage.tsx
@@ -11,28 +11,29 @@ interface RichImageProps {
 
 export default function RichImage({ asset }: RichImageProps) {
   const [imageLoaded, setImageLoaded] = useState(false)
-  const imageRef = useRef<HTMLImageElement>(null!)
+  const imageRef = useRef<HTMLImageElement>(null)
   const url = asset ? `https:${asset?.fields.file?.url}` : placeholderImage
   const description = asset?.fields.description
 
-  const handleImageLoaded = () => {
-    setImageLoaded(true)
-  }
-
   useEffect(() => {
-    imageRef.current.onload = () => setImageLoaded(true)
-    if (imageRef.current.complete) setImageLoaded(true)
+    const img = imageRef.current
+    if (img) {
+      img.onload = () => setImageLoaded(true)
+      if (img.complete) setImageLoaded(true)
+      return () => {
+        img.onload = null
+      }
+    }
   }, [imageRef])
 
   return (
     <figure className="relative m-0 w-full h-auto">
-      { !imageLoaded && (
-        <div className="aspect-video h-auto flex justify-center items-center border w-full rounded-lg">
-          <div className="relative h-16 w-16 border-[6px] border-solid border-r-transparent border-border rounded-full animate-spin" />
-        </div>
-      )}
-      <picture 
-        className={cn('flex justify-center items-center w-full h-auto animate-fade-in transition-all opacity-100')}>
+      <picture className="relative w-full h-auto">
+        { !imageLoaded && (
+          <div className="absolute top-0 bottom-0 right-0 left-0 aspect-video h-auto flex justify-center items-center border w-full rounded-lg">
+            <div className="relative h-16 w-16 border-[6px] border-solid border-r-transparent border-border rounded-full animate-spin" />
+          </div>
+        )}
         <Image 
           src={ url }
           alt={ description ?? '' }
@@ -40,8 +41,10 @@ export default function RichImage({ asset }: RichImageProps) {
           height={ asset?.fields.file?.details.image?.width }
           sizes="(max-width: 828px) calc(100vw - 16px), 775.5"
           ref={ imageRef }
-          onLoad={ handleImageLoaded }
-          className={cn('flex justify-center items-center w-full h-full rounded-lg animate-fade-in')}
+          onLoad={ () => setImageLoaded(true) }
+          className={cn('flex justify-center items-center w-full h-full aspect-video rounded-lg', {
+            'animate-fade-in': imageLoaded
+          })}
         />
       </picture>
       <figcaption className="flex justify-center items-center mt-2 mb-4 w-auto italic px-4 xl:px-8">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,13 +70,13 @@ const config = {
         },
         "fade-in": {
           from: { opacity: '0' },
-          to: { opacity: '100' }
+          to: { opacity: '1' }
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "fade-in": "fade-in 0.2s ease-in-out",
+        "fade-in": "fade-in 0.5s ease-in-out",
       },
     },
   },
