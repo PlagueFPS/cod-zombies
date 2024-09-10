439cd1cd5d023ab31a0b5a9555faa4328628d3fa
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-10 08:04:08 -0400

    Added `<RichLink />`

---
+++ components/RichText/RichLink/RichLink.tsx
@@ -0,0 +1,43 @@
+import { WEBSITE_URL } from "@/utils/constants"
+import { getYouTubeVideoID } from "@/utils/functions"
+import { YouTubeEmbed } from "@next/third-parties/google"
+import Link from "next/link"
+
+interface RichLinkProps {
+  node: any
+}
+
+const youtube_url = 'https://youtu.be/'
+const dev_url = 'http://localhost:3000'
+
+export default function RichLink({ node }: RichLinkProps) {
+  if (node.data.uri.startsWith(youtube_url)) {
+    return (
+      <>
+        <h3 className='text-foreground font-semibold mb-4'>{ node.content[0].value }</h3>
+        <YouTubeEmbed videoid={ getYouTubeVideoID(node.data.uri) ?? '' } style='border-radius: var(--radius);'  />
+      </>
+    )
+  }
+  else if (node.data.uri.startsWith(WEBSITE_URL)) {
+    return (
+      <Link href={ node.data.uri }>
+        { node.content[0].value }
+      </Link>
+    )
+  }
+  else if (node.data.uri.startsWith(dev_url)) {
+    return (
+      <Link href={ node.data.uri.replace(dev_url, WEBSITE_URL) }>
+        { node.content[0].value }
+      </Link>
+    )
+  }
+  else {
+    return (
+      <a href={ node.data.uri } target='_blank' rel='noopener noreferrer'>
+        { node.content[0].value }
+      </a>
+    )
+  }
+}
--- components/RichText/RichTextRenderer/RichTextRenderer.tsx
+++ components/RichText/RichTextRenderer/RichTextRenderer.tsx
@@ -1,9 +1,6 @@
 import { Document, INLINES, BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import { YouTubeEmbed } from "@next/third-parties/google"
-import { getYouTubeVideoID, slugify } from "@/utils/functions"
-import { WEBSITE_URL } from "@/utils/constants"
-import Link from "next/link"
+import { slugify } from "@/utils/functions"
 import RichImage from "../RichImage/RichImage"
 import Heading2 from "../RichHeadings/Heading2/Heading2"
 import Heading3 from "../RichHeadings/Heading3/Heading3"
@@ -11,48 +8,18 @@ import GKValve from "../RichEmbeds/GKValve"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import RichBlockquote from "../RichBlockquote/RichBlockquote"
 import React from "react"
+import RichLink from "../RichLink/RichLink"
 
 interface RichTextRendererProps {
   body: Document
   slug: string
 }
 
-const youtube_url = 'https://youtu.be/'
-const dev_url = 'http://localhost:3000'
-
 export default function RichTextRenderer({ body, slug }: RichTextRendererProps) {
   const renderOptions = {
     renderNode: {
       [INLINES.HYPERLINK]: (node: any) => {
-        if (node.data.uri.startsWith(youtube_url)) {
-          return (
-            <>
-              <h3 className='text-foreground font-semibold mb-4'>{ node.content[0].value }</h3>
-              <YouTubeEmbed videoid={ getYouTubeVideoID(node.data.uri) ?? '' } style='border-radius: var(--radius);'  />
-            </>
-          )
-        }
-        else if (node.data.uri.startsWith(WEBSITE_URL)) {
-          return (
-            <Link href={ node.data.uri }>
-              { node.content[0].value }
-            </Link>
-          )
-        }
-        else if (node.data.uri.startsWith(dev_url)) {
-          return (
-            <Link href={ node.data.uri.replace(dev_url, WEBSITE_URL) }>
-              { node.content[0].value }
-            </Link>
-          )
-        }
-        else {
-          return (
-            <a href={ node.data.uri } target='_blank' rel='noopener noreferrer'>
-              { node.content[0].value }
-            </a>
-          )
-        }   
+        return <RichLink node={ node } />
       },
       [BLOCKS.EMBEDDED_ASSET]: (node: any) => {
         const asset = node.data.target
