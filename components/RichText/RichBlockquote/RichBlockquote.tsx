47cd3eba4bcc8c4409d4685168dd675d4808b934
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-10 07:44:07 -0400

    Updated `<RichBlockquote />`

--- components/RichText/RichBlockquote/RichBlockquote.tsx
+++ components/RichText/RichBlockquote/RichBlockquote.tsx
@@ -1,3 +1,5 @@
+import { AlertCircle } from "lucide-react"
+
 interface BlockquoteProps {
   children: React.ReactNode
 }
@@ -5,13 +7,19 @@ interface BlockquoteProps {
 export default function RichBlockquote({ children }: BlockquoteProps) {
   return (
     <blockquote className="space-y-6 p-4">
-      <div className="my-8 overflow-hidden rounded-xl bg-gradient-to-tl from-orange-100 via-orange-50 to-white p-[1px] shadow-lg dark:from-orange-700 dark:to-orange-800">
-      <div className="rounded-xl bg-gradient-to-tl from-white to-orange-50 py-3 px-6 backdrop-blur-sm dark:from-black dark:via-black dark:to-orange-900">
-        <div className="text-orange-700 dark:text-orange-300">
-          {children}
+      <div className="my-8 overflow-hidden rounded-xl bg-gradient-to-br from-orange-100 via-orange-50 to-white p-[1px] shadow-lg dark:from-orange-700 dark:to-orange-800">
+        <div className="rounded-xl bg-gradient-to-br from-orange-50 via-white to-orange-50 p-6 backdrop-blur-sm dark:from-orange-950 dark:via-black dark:to-orange-950">
+          <div className="flex items-center gap-3 mb-4">
+            <AlertCircle className="h-5 w-5 text-orange-700 dark:text-orange-400" aria-hidden="true" />
+            <div className="text-lg font-bold bg-clip-text text-transparent bg-gradient-to-b from-orange-700 to-orange-900 dark:from-orange-200 dark:via-orange-300 dark:to-orange-400">
+              Important Note
+            </div>
+          </div>
+          <div className="text-orange-700 dark:text-orange-300">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
     </blockquote>
   )
 }
--- components/RichText/RichTextRenderer/RichTextRenderer.tsx
+++ components/RichText/RichTextRenderer/RichTextRenderer.tsx
@@ -126,6 +126,13 @@ export default function RichTextRenderer({ body, slug }: RichTextRendererProps) 
           </TableCell>
         )
       },
+    },
+    renderMark: {
+      [MARKS.ITALIC]: (text: any) => {
+        console.log(text)
+        if (text?.props?.children === 'Important Note: ') return null
+        else return <i>{ text }</i>
+      }
     }
   }
 
