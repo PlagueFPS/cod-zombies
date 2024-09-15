ad328068981713032fe6c88074942d17dbe5648c
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-15 07:43:36 -0400

    Removed redundant `loading.tsx` files

--- app/(Home)/loading.tsx
+++
@@ -1,5 +0,0 @@
-import HomePageLoader from '@/components/Loaders/HomePageLoader'
-
-export default function HomePageLoading() {
-  return <HomePageLoader />
-}
--- app/[category]/loading.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { useParams } from "next/navigation";
-import HomePageLoader from "@/components/Loaders/HomePageLoader";
-import { checkParams } from "@/utils/functions";
-
-export default function CategoryPageLoader() {
-  const { category } = useParams()
-  return <HomePageLoader category={ checkParams(category) ?? undefined } />
-}
