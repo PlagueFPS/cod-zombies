0c9b25767b3d9cbf064bb31112f0d690f27f6b56
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-16 19:30:59 -0400

    removed useless comment

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,6 @@ import ThemeToggleLoader from "../Loaders/ThemeToggleLoader"
 import Newsletter from "../Newsletter/Newsletter"
 
 // dynamic import to avoid possible hydration error during year flip
-// won't be an issue with Next 15 due to PPR & unstable_noStore()
 const Copyright = dynamic(() => import('@/components/Footer/Copyright/Copyright'), {
   ssr: false,
   loading: () => <div>&copy; { new Date().getFullYear() }</div>
@@ -29,25 +28,6 @@ export default function Footer() {
           <ThemeToggle />
         </div>
       </div>
-      {/* <Newsletter />
-      <div className="flex flex-col sm:flex-row gap-4 justify-between items-center w-full">
-        <div className="flex flex-col gap-2 items-center xl:items-start justify-center text-xs text-muted-foreground max-w-sm">
-          <div className="flex gap-1.5 justify-center items-center">
-            <Copyright />
-            <div className="flex gap-1">
-              Call of Duty:
-              <span>Zombies Guides</span>
-            </div>
-          </div>
-          <span className="tracking-tight">
-            All content, including but not limited to images, logos, and trademarks related to Activision games, are property of Activision Publishing, Inc. and its respective owners.
-          </span>
-        </div>
-        <div className="flex gap-8 justify-center items-center border-b pb-4 sm:border-none sm:pb-0">
-          <ContactForm />
-          <ThemeToggle />
-        </div>
-      </div> */}
     </footer>
   )
 }
