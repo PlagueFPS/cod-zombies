f1a6d50215fbaec35de6748537b069db106d7acf
Angel P <anticrebel@gmail.com>
2024-09-23 14:44:34 -0400

    Fix: table of contents z-index

--- components/TableOfContents/TableOfContents.tsx
+++ components/TableOfContents/TableOfContents.tsx
@@ -19,7 +19,7 @@ export default function TableOfContents({ headings }: TableOfContentsProps) {
   return (
     <>
       { isDesktop ? (
-        <aside className='sticky top-4 ml-4 flex-shrink-0 w-[340px] h-fit border rounded-lg px-6'>
+        <aside className='sticky top-4 ml-4 z-40 flex-shrink-0 w-[340px] h-fit border rounded-lg px-6'>
           <nav className="flex flex-col gap-4 border-b pb-3">
             <div className="font-bold mx-auto mt-4">On this page</div>
             <ScrollArea ref={ scrollAreaRef } className="h-[70vh]">
@@ -27,7 +27,7 @@ export default function TableOfContents({ headings }: TableOfContentsProps) {
                 { headings.map(heading => (
                   <li 
                     key={ heading.id }
-                    className={cn("hover:text-primary w-fit transition-all", 
+                    className={cn("hover:text-primary w-fit transition-colors", 
                       { 'pl-3 text-foreground/70 font-medium': heading.type === 'heading-3',
                         'pl-6 text-muted-foreground font-normal': heading.type === 'heading-4',
                         'text-primary': activeHeading === heading.id,
