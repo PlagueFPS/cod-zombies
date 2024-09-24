e7aa9b6a31b05cc8469339876693401ce4d337fe
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-24 00:49:49 -0400

    update: dark theme metadata color

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
-    { media: '(prefers-color-scheme: dark)', color: '#0c0a09' },
+    { media: '(prefers-color-scheme: dark)', color: '#09090b' },
   ]
 }
 
--- components/Newsletter/NewsletterForm.tsx
+++ components/Newsletter/NewsletterForm.tsx
@@ -5,6 +5,7 @@ import { toast } from "sonner"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
+import { Loader2 } from "lucide-react"
 
 export default function NewsletterForm() {
   const [state, action, pending] = useActionState(subscribeToNewsletter, { success: false })
@@ -31,13 +32,13 @@ export default function NewsletterForm() {
             required
             className="pr-28 rounded-sm"
           />
-          <Button 
-            type="submit"
-            variant={"secondary"}
-            className="absolute right-1 top-1/2 transform -translate-y-1/2 h-[calc(100%-0.5rem)] rounded-sm px-3 text-sm"
-            disabled={ pending }
-          >
-            { pending ? 'Subscribing...' : 'Subscribe'}
+          <Button type="submit" variant={"secondary"} className="absolute right-1 top-1/2 transform -translate-y-1/2 h-[calc(100%-0.5rem)] rounded-sm px-3 text-sm" disabled={ pending }>
+            { pending ? (
+              <div className="flex items-center gap-2">
+                <Loader2 className="w-4 h-4 animate-spin" />
+                Subscribing...
+              </div>
+            ) : 'Subscribe' }
           </Button>
         </div>
       </div>
