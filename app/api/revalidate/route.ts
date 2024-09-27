ff36b709dca71f726c629f9286df913c90743d86
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-27 16:53:53 -0400

    refactor: revalidate API to handle category revalidation

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,69 +1,108 @@
 import type { NextRequest } from "next/server"
 import { headers } from "next/headers"
-import { revalidatePath } from "next/cache"
+import { timingSafeEqual } from "crypto"
+import { TextEncoder } from "util"
+import { revalidatePath, revalidateTag } from "next/cache"
 import { getFeaturedMapById } from "@/data/featuredMaps"
+import { getGameCategoryById } from "@/data/gameCategory"
 import { env } from "@/env"
 import { ContentfulWebhookBodySchema } from "@/utils/validationSchemas"
 import { isFirstTimePublish } from "@/utils/contentful-utils"
+import { CACHE_KEYS } from "@/utils/constants"
 
 export async function PUT(req: NextRequest) {
   const headersList = await headers()
   const secret = headersList.get('X-Contentful-Revalidate-Secret')
+  const encoder = new TextEncoder()
+  const secretBuffer = encoder.encode(secret || '')
+  const validSecretBuffer = encoder.encode(env.REVALIDATE_SECRET)
  
-  if (secret !== env.REVALIDATE_SECRET) {
+  if (!timingSafeEqual(secretBuffer, validSecretBuffer)) {
     return Response.json({ revalidated: false, message: 'Unauthorized Request' }, { status: 401 })
   }
 
-  const bodyValidation = ContentfulWebhookBodySchema.safeParse(await req.json())
-  if (!bodyValidation.success) {
-    return Response.json({ revalidated: false, message: 'Invalid Request Body' }, { status: 400 })
+  const payload = ContentfulWebhookBodySchema.safeParse(await req.json())
+  if (!payload.success) {
+    return Response.json({ revalidated: false, message: 'Invalid Request Body', errors: payload.error.message }, { status: 400 })
   }
 
-  const { mapId, createdAt, updatedAt } = bodyValidation.data
-  // Manually setting draftMode to false to prevent trying to revalidate draft content
-  const map = await getFeaturedMapById(false, mapId)
-  if (!map) return Response.json({ revalidated: false, message: 'Map Not Found' }, { status: 404 })
+  switch (payload.data.type) {
+    case 'map': {
+      const { mapId, createdAt, updatedAt } = payload.data
+      // Manually setting draftMode to false to prevent trying to revalidate draft content
+      const map = await getFeaturedMapById(false, mapId)
+      if (!map) return Response.json({ revalidated: false, message: 'Map Not Found' }, { status: 404 })
 
-  const category = map.gameCategory
-  
-  if (isFirstTimePublish(createdAt, updatedAt)) { // revalidate the category page if it is a newly created map
-    const categoryPath = `/${category?.fields.slug}`
-    revalidatePath(categoryPath)
+      const category = map.gameCategory
+        
+      if (isFirstTimePublish(createdAt, updatedAt)) { // revalidate the category page if it is a newly created map
+      const categoryPath = `/${category?.fields.slug}`
+      revalidatePath(categoryPath)
 
-    return Response.json({ revalidated: true, message: `${categoryPath} Revalidated` }, { status: 201 })
-  }
-  else { // revalidate the map page if it is an updated map
-    const mapPath = `/${category?.fields.slug}/${map.slug}`
-    revalidatePath(mapPath)
-    
-    return Response.json({ revalidated: true, message: `${mapPath} Revalidated` }, { status: 201 })
+      return Response.json({ revalidated: true, message: `${categoryPath} Revalidated` }, { status: 201 })
+      }
+      else { // revalidate the map page if it is an updated map
+        const mapPath = `/${category?.fields.slug}/${map.slug}`
+        revalidatePath(mapPath)
+        
+        return Response.json({ revalidated: true, message: `${mapPath} Revalidated` }, { status: 201 })
+      }
+    }  
+    case 'category': {
+      const { categoryId } = payload.data
+      // Manually setting draftMode to false to prevent trying to revalidate draft content
+      const category = await getGameCategoryById(false, categoryId)
+      if (!category) return Response.json({ revalidated: false, message: 'Category Not Found' }, { status: 404 })
+      // Invalidate the category data set
+      revalidateTag(CACHE_KEYS.GAME_CATEGORIES)
+      return Response.json({ revalidated: true, message: `${CACHE_KEYS.GAME_CATEGORIES} Revalidated` }, { status: 201 })
+    }
   }
 }
 
 export async function DELETE(req: NextRequest) {
+  // This endpoint is for removing unpublished maps via revalidation from the frontend
+  // We do not delete any data from Contentful or a Database
   const headersList = await headers()
   const secret = headersList.get('X-Contentful-Revalidate-Secret')
+  const encoder = new TextEncoder()
+  const secretBuffer = encoder.encode(secret || '')
+  const validSecretBuffer = encoder.encode(env.REVALIDATE_SECRET)
  
-  if (secret !== env.REVALIDATE_SECRET) {
+  if (!timingSafeEqual(secretBuffer, validSecretBuffer)) {
     return Response.json({ deleted: false, message: 'Unauthorized Request' }, { status: 401 })
   }
   
-  const bodyValidation = ContentfulWebhookBodySchema.safeParse(await req.json())
-  if (!bodyValidation.success) {
-    return Response.json({ deleted: false, message: 'Invalid Request Body' }, { status: 400 })
+  const payload = ContentfulWebhookBodySchema.safeParse(await req.json())
+  if (!payload.success) {
+    return Response.json({ deleted: false, message: 'Invalid Request Body', errors: payload.error.message }, { status: 400 })
   }
 
-  const { mapId } = bodyValidation.data
-  // Manually setting draftMode to true to be able to remove unpublished maps
-  const map = await getFeaturedMapById(true, mapId)
-  if (!map) return Response.json({ deleted: false, message: 'Map Not Found' }, { status: 404 })
+  switch (payload.data.type) {
+    case 'map': {
+      const { mapId } = payload.data
+      // Manually setting draftMode to true to be able to remove unpublished maps
+      const map = await getFeaturedMapById(true, mapId)
+      if (!map) return Response.json({ deleted: false, message: 'Map Not Found' }, { status: 404 })
 
-  const category = map.gameCategory
-  const mapPath = `/${category?.fields.slug}/${map.slug}`
-  const categoryPath = `/${category?.fields.slug}`
-  // revalidate anywhere the map exists to remove it
-  revalidatePath(mapPath)
-  revalidatePath(categoryPath)
+      const category = map.gameCategory
+      const mapPath = `/${category?.fields.slug}/${map.slug}`
+      const categoryPath = `/${category?.fields.slug}`
+      // revalidate anywhere the map exists to remove it
+      revalidatePath(mapPath)
+      revalidatePath(categoryPath)
 
-  return Response.json({ deleted: true, message: `${mapPath} and ${categoryPath} Revalidated` }, { status: 200 })
+      return Response.json({ deleted: true, message: `${mapPath} and ${categoryPath} Revalidated` }, { status: 200 })
+    }
+    case 'category': {
+      const { categoryId } = payload.data
+      // Manually setting draftMode to true to be able to remove unpublished categories
+      const category = await getGameCategoryById(true, categoryId)
+      if (!category) return Response.json({ deleted: false, message: 'Category Not Found' }, { status: 404 })
+
+      // Invalidate the category data set
+      revalidateTag(CACHE_KEYS.GAME_CATEGORIES)
+      return Response.json({ deleted: true, message: `${CACHE_KEYS.GAME_CATEGORIES} Revalidated` }, { status: 200 })
+    }
+  }
 }
--- utils/validationSchemas.ts
+++ utils/validationSchemas.ts
@@ -15,14 +15,30 @@ export const FeedbackFormSchema = zfd.formData({
 })
 
 export const NewsletterFormSchema = zfd.formData({
-  email: zfd.text(z.string().email()),
+  email: zfd.text(z.string().email({ message: "Invalid email address" })),
 })
 
-export const ContentfulWebhookBodySchema = z.object({
-  mapId: z.string().min(1),
-  createdAt: z.string().datetime(),
-  updatedAt: z.string().datetime(),
-})
+// Base schema for common fields
+const BaseContentfulWebhookBodySchema = z.object({
+  createdAt: z.string({ required_error: "Created At is required" }).datetime(),
+  updatedAt: z.string({ required_error: "Updated At is required" }).datetime(),
+});
+
+// Schema for payloads with mapId
+export const MapContentfulWebhookBodySchema = BaseContentfulWebhookBodySchema.extend({
+  mapId: z.string({ required_error: "Map Id is required" }).min(1),
+});
+
+// Schema for payloads with categoryId
+export const CategoryContentfulWebhookBodySchema = BaseContentfulWebhookBodySchema.extend({
+  categoryId: z.string({ required_error: "Category Id is required" }).min(1),
+});
+
+// Union type to represent either case
+export const ContentfulWebhookBodySchema = z.discriminatedUnion('type', [
+  MapContentfulWebhookBodySchema.extend({ type: z.literal('map') }),
+  CategoryContentfulWebhookBodySchema.extend({ type: z.literal('category') }),
+]);
 
 const stringOrStringArray = z.union([z.string(), z.array(z.string())])
 
