d0cc1c33a537e58a662fbb3369d7c09897443ed0
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-09-30 01:43:56 -0400

    fix: removed try/catch blocks blocking the `PPR` proccess

--- data/kv.ts
+++ data/kv.ts
@@ -5,43 +5,19 @@ import { cache } from "react"
 const oneWeek = 7 * 24 * 60 * 60 // 1 week in seconds
 
 export const storeNewMapId = cache(async (mapId: string, createdAt: string) => {
-  try {
-    await kv.set(`${NEW_MAP_PREFIX}${mapId}`, createdAt, { ex: oneWeek, nx: true })
-  } catch (error) {
-    console.error(`[KV] Error storing new map id: ${mapId}`, error)
-  }
+  await kv.set(`${NEW_MAP_PREFIX}${mapId}`, createdAt, { ex: oneWeek, nx: true })
 })
 
 export const getAllNewMapIds = cache(async () => {
-  try {
-    const keys = await kv.keys(`${NEW_MAP_PREFIX}*`)
-    if (keys.length > 0) {
-      return new Set(keys.map(key => key.replace(NEW_MAP_PREFIX, "")))
-    }
-    return new Set<string>()
-  } catch (error) {
-    console.error(`[KV] Error getting all new map ids`, error)
-    return new Set<string>()
-  }
+  const keys = await kv.keys(`${NEW_MAP_PREFIX}*`)
+  return new Set(keys.map(key => key.replace(NEW_MAP_PREFIX, "")))
 })
 
 export const storeNewCategoryId = cache(async (categoryId: string, createdAt: string) => {
-  try {
-    await kv.set(`${NEW_CATEGORY_PREFIX}${categoryId}`, createdAt, { ex: oneWeek, nx: true })
-  } catch (error) {
-    console.error(`[KV] Error storing new category id: ${categoryId}`, error)
-  }
+  await kv.set(`${NEW_CATEGORY_PREFIX}${categoryId}`, createdAt, { ex: oneWeek, nx: true })
 })
 
 export const getAllNewCategoryIds = cache(async () => {
-  try {
-    const keys = await kv.keys(`${NEW_CATEGORY_PREFIX}*`)
-    if (keys.length > 0) {
-      return new Set(keys.map(key => key.replace(NEW_CATEGORY_PREFIX, "")))
-    }
-    return new Set<string>()
-  } catch (error) {
-    console.error(`[KV] Error getting all new category ids`, error)
-    return new Set<string>()
-  }
+  const keys = await kv.keys(`${NEW_CATEGORY_PREFIX}*`)
+  return new Set(keys.map(key => key.replace(NEW_CATEGORY_PREFIX, "")))
 })
