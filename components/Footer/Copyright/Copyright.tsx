87edd33dff25602d7c2566f3f31200334d897232
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-10-01 06:09:18 -0400

    upgrade: `next` to `canary.174`

--- components/Footer/Copyright/Copyright.tsx
+++ components/Footer/Copyright/Copyright.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@/lib/utils"
 import { DetailedHTMLProps, HTMLAttributes } from "react"
-import { unstable_noStore } from "next/cache"
+import { connection } from "next/server"
 
 interface CopyrightProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
   className?: string
 }
 
-export default function Copyright({ className }: CopyrightProps) {
-  unstable_noStore()
+export default async function Copyright({ className }: CopyrightProps) {
+  await connection()
   return (
     <p className={cn("text-sm text-muted-foreground", className)}>
       &copy; { new Date().getFullYear() } Call of Duty: Zombies Guides
