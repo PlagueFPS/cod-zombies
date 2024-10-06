49f158eaf125603cb6b8633f7cf8ece12d5f72e7
Plague <anticrebel@gmail.com>
2024-10-06 04:29:22 -0400

    fix: unability to submit form via enter key

--- components/FeedbackForm/FeedbackForm.tsx
+++ components/FeedbackForm/FeedbackForm.tsx
@@ -75,125 +75,127 @@ export default function FeedbackForm({ className }: FeedbackFormProps) {
               We appreciate your feedback. Please fill out the form below.
             </DialogDescription>
           </DialogHeader>
-          <ScrollArea className="max-h-[70dvh] pr-4">
-            <Form {...form}>
-              <form onSubmit={ handleSubmitWithAction } className="space-y-6">
-                <FormField 
-                  control={ form.control }
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input 
-                          {...field} 
-                          required 
-                          placeholder="Enter a title for your feedback" 
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        This could be something like, maps, user experience, visuals, etc.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField 
-                  control={ form.control }
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email (optional)</FormLabel>
-                      <FormControl>
-                        <Input 
-                          {...field}
-                          type="email" 
-                          placeholder="your@email.com"
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        The email we will use to potentially contact you.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField 
-                  control={ form.control }
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name (optional)</FormLabel>
-                      <FormControl>
-                        <Input 
-                          {...field}
-                          placeholder="Enter your name" 
-                        />
-                      </FormControl>
-                      <FormDescription className="">
-                        The name we will use to address you via email.
-                      </FormDescription>
-                    </FormItem>
-                  )}
-                />
-                <FormField 
-                  control={ form.control }
-                  name="label"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Feedback Label</FormLabel>
-                      <Select required onValueChange={ field.onChange } defaultValue={ field.value }>
+          <Form {...form}>
+            <form onSubmit={ handleSubmitWithAction }>
+              <ScrollArea className="h-[60dvh] pr-4">
+                <div className="space-y-6 pb-4">
+                  <FormField 
+                    control={ form.control }
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
                         <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a label" />
-                          </SelectTrigger>
+                          <Input 
+                            {...field} 
+                            required 
+                            placeholder="Enter a title for your feedback" 
+                          />
                         </FormControl>
-                        <SelectContent>
-                          <SelectItem value="featureRequest">Feature Request</SelectItem>
-                          <SelectItem value="issue">Issue</SelectItem>
-                          <SelectItem value="question">Question</SelectItem>
-                          <SelectItem value="idea">Idea</SelectItem>
-                          <SelectItem value="complaint">Complaint</SelectItem>
-                          <SelectItem value="other">Other</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormDescription>
-                        Choose a label that best describes your feedback.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField 
-                  control={ form.control }
-                  name="feedback"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Your Feedback</FormLabel>
-                      <FormControl>
-                        <Textarea 
-                          {...field}
-                          required
-                          placeholder="What can we improve?"
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        Please provide constructive and actionable feedback to help us improve.
-                      </FormDescription>
-                    </FormItem>
-                  )}
-                />
-              </form>
-            </Form>
-          </ScrollArea>
-          <Button type="submit" onClick={ handleSubmitWithAction } className="w-full" disabled={ isPending }>
-            { isPending ? (
-              <div className="flex items-center gap-2">
-                <Loader2 className="w-4 h-4 animate-spin" />
-                Submitting...
-              </div>
-            ) : 'Submit Feedback' }
-          </Button>
+                        <FormDescription>
+                          This could be something like, maps, user experience, visuals, etc.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    control={ form.control }
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email (optional)</FormLabel>
+                        <FormControl>
+                          <Input 
+                            {...field}
+                            type="email" 
+                            placeholder="your@email.com"
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          The email we will use to potentially contact you.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    control={ form.control }
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name (optional)</FormLabel>
+                        <FormControl>
+                          <Input 
+                            {...field}
+                            placeholder="Enter your name" 
+                          />
+                        </FormControl>
+                        <FormDescription className="">
+                          The name we will use to address you via email.
+                        </FormDescription>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    control={ form.control }
+                    name="label"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Feedback Label</FormLabel>
+                        <Select required onValueChange={ field.onChange } defaultValue={ field.value }>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select a label" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="featureRequest">Feature Request</SelectItem>
+                            <SelectItem value="issue">Issue</SelectItem>
+                            <SelectItem value="question">Question</SelectItem>
+                            <SelectItem value="idea">Idea</SelectItem>
+                            <SelectItem value="complaint">Complaint</SelectItem>
+                            <SelectItem value="other">Other</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormDescription>
+                          Choose a label that best describes your feedback.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    control={ form.control }
+                    name="feedback"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Your Feedback</FormLabel>
+                        <FormControl>
+                          <Textarea 
+                            {...field}
+                            required
+                            placeholder="What can we improve?"
+                          />
+                        </FormControl>
+                        <FormDescription>
+                          Please provide constructive and actionable feedback to help us improve.
+                        </FormDescription>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </ScrollArea>
+              <Button type="submit" className="w-full" disabled={ isPending }>
+                { isPending ? (
+                  <div className="flex items-center gap-2">
+                    <Loader2 className="w-4 h-4 animate-spin" />
+                    Submitting...
+                  </div>
+                ) : 'Submit Feedback' }
+              </Button>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
