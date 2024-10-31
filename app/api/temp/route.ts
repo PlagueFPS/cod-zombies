e97e6facc60b8c04001790383685d9e3921d8f5c
Plague <74983533+PlagueFPS@users.noreply.github.com>
2024-10-31 14:15:52 -0400

    removed: temp route

--- app/api/temp/route.ts
+++
@@ -1,7 +0,0 @@
-import { CACHE_KEYS } from "@/utils/constants";
-import { revalidateTag } from "next/cache";
-
-export function GET() {
-  revalidateTag(`${CACHE_KEYS.FEATURED_MAPS.ALL}`)
-  return Response.json({ revalidated: true, message: `Revalidated: ${CACHE_KEYS.FEATURED_MAPS.ALL}`}, { status: 200 })
-}
