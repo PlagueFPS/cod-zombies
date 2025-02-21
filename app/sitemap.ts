95404a23176354e89cfefdb2ca679cf3565b5c81
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-02-21 09:59:52 -0500

    update: sitemap to reflect canonical urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,60 +1,27 @@
 import { MetadataRoute } from "next";
 import { getMaps } from "@/data/maps";
-import { getGames } from "@/data/games";
 import { env } from "@/env";
 import { getQuests } from "@/data/sideQuests";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const mapsPromise = getMaps(false)
-  const gamesPromise = getGames(false)
   const questsProimse = getQuests(false)
-  const [maps, games, quests] = await Promise.all([mapsPromise, gamesPromise, questsProimse])
+  const [maps, quests] = await Promise.all([mapsPromise, questsProimse])
   const searchMaps = maps.filter(map => !map.isComingSoon)
-
-  const generateGameDate = (slug: string, type: "maps" | "quests") => {
-    let date: typeof maps[0]["updatedAt"] | undefined
-    
-    if (type === "maps") {
-      date = searchMaps.find(m => m.category.slug === slug)?.updatedAt
-    } else if (type === "quests") {
-      date = quests.find(q => q.game.slug === slug)?.updatedAt
-    }
-
-    if (date) return new Date(date)
-    else return undefined
-  }
-
-  const generateMapDate = (slug: string) => {
-    const date = quests.find(q => q.map.slug === slug)?.updatedAt
-    if (date) return new Date(date)
-    else return undefined
-  }
 
   return [
     {
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}`,
       lastModified: new Date(maps[0].updatedAt),
     },
-    {
-      url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests`,
-      lastModified: new Date(quests[0].updatedAt)
-    },
-    ...games.map(game => ({
-      url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${game.slug}`,
-      lastModified: generateGameDate(game.slug, "maps")
-    })),
     ...searchMaps.map(map => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${map.category.slug}/${map.slug}`,
       lastModified: new Date(map.updatedAt)
     })),
-    ...games.map(g => ({
-      url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests/${g.slug}`,
-      lastModified: generateGameDate(g.slug, "quests")
-    })),
-    ...searchMaps.map(m => ({
-      url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests/${m.category.slug}/${m.slug}`,
-      lastModified: generateMapDate(m.slug)
-    })),
+    {
+      url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests`,
+      lastModified: new Date(quests[0].updatedAt)
+    },
     ...quests.map(q => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests/${q.game.slug}/${q.map.slug}/${q.slug}`,
       lastModified: new Date(q.updatedAt),
