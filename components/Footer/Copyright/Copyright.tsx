90e7ec73ff0b7bc9d918e0ef929d844d7fdd4202
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-02-24 09:33:48 -0500

    update: copyright styling

--- components/Footer/Copyright/Copyright.tsx
+++ components/Footer/Copyright/Copyright.tsx
@@ -13,7 +13,7 @@ export default async function Copyright({ className }: CopyrightProps) {
       <p>
         &copy; { new Date().getFullYear() } Call of Duty: Zombies Guides
       </p>
-      <p>
+      <p className="md:pr-12">
         This website is an independent, unofficial Call of Duty: Zombies fan site. It is not affiliated with or endorsed by Activision Blizzard. All trademarks, service marks, trade names, trade dress, product names, and logos appearing on this site are the property of their respective owners.
       </p>
     </div>
