49c2b0bd315b8801cb82101f6ba6043356e21123
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-04-15 01:53:33 -0400

    task: finalize zombie preview card

--- components/BestiaryGrid/BestiaryGrid.client.tsx
+++ components/BestiaryGrid/BestiaryGrid.client.tsx
@@ -6,7 +6,7 @@ import { calculateSkip } from "@/utils/contentful-utils"
 import { Suspense, useEffect, useState } from "react"
 import FeaturedImage from "../FeaturedImage/FeaturedImage"
 import { Badge } from "../ui/badge"
-import { Card, CardContent, CardDescription, CardTitle } from "../ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
 import { ChangedBadge, DraftBadge, NewBadge, TypeBadge } from "../CustomBadges/CustomBadges"
 import { CustomLink } from "../CustomLink/CustomLink"
 import GridPagination from "../GridPagination/GridPagination"
@@ -76,30 +76,49 @@ function ZombiePreviewCard({ zombie, zombieIndex, draftMode }: IZombiePreviewCar
   const priority = zombieIndex === 0
   const alt = `${zombie.name} Image`
 
+
   return (
-    <article className="h-full group outline-none">
-      <CustomLink href={`/bestiary/${zombie.slug}`} aria-label={`Go to ${zombie.name} page`}>
-        <Card className="overflow-hidden w-full transition-all shadow-lg hover:shadow-primary bg-background">
-          <div className="relative w-full border-b overflow-hidden">
+    <article className="max-h-[450px] h-full group outline-none">
+      <CustomLink href={ `/bestiary/${zombie.slug}` } aria-label={ `View details for ${zombie.name}` }>
+        <Card className={`
+          relative h-full group-hover:border-primary group-hover:scale-105 
+          group-focus-visible:scale-105 group-focus-visible:border-primary 
+          cursor-pointer transition-transform overflow-hidden animate-fade-in 
+          shadow-xl dark:shadow-none`}
+        >
+          <div className='absolute top-2 right-2 z-20 w-fit flex items-center justify-center gap-1'>
+            { zombie.isNew ? <NewBadge /> : null }
+            { (draftMode || IN_DEVELOPMENT) && zombie.isDraft ? <DraftBadge /> : null }
+            { (draftMode || IN_DEVELOPMENT) && zombie.isChanged ? <ChangedBadge /> : null }
+            <TypeBadge type={ zombie.type } />
+            <Badge className='badge-primary-gradient'>
+              { zombie.games[0].title }
+            </Badge>
+          </div>
+          <div className="hidden dark:flex absolute inset-0 z-10 items-center w-full h-full opacity-25 blur-2xl">
             <FeaturedImage 
-              featuredImage={ zombie.image }
-              priority={ priority }
-              alt={ alt }
-              sizes="280px"
-              className="h-60 object-top object-cover transition-transform duration-700 group-hover:scale-105"
+              featuredImage={ zombie.image } 
+              priority={ priority } 
+              quality={ 1 }
+              sizes='32px'
+              className='aspect-square scale-150'
             />
           </div>
-          <CardContent className="p-4">
-            <div className="flex flex-wrap gap-2 items-center">
-              { zombie.isNew ? <NewBadge /> : null }
-              { (draftMode || IN_DEVELOPMENT) && zombie.isDraft ? <DraftBadge /> : null }
-              { (draftMode || IN_DEVELOPMENT) && zombie.isChanged ? <ChangedBadge /> : null }
-              <TypeBadge type={ zombie.type } />
-              <Badge className="badge-primary-gradient">{ zombie.games[0].title }</Badge>
+          <CardHeader className="flex gap-2 flex-grow">
+            <div className='relative overflow-hidden h-full w-full rounded-md'>
+              <FeaturedImage 
+                featuredImage={ zombie.image } 
+                priority={ priority }
+                alt={ alt }
+                sizes='272px'
+                className="h-44 object-cover object-top"
+              />
             </div>
-            <CardTitle className="font-extrabold text-2xl mt-2 text-gradient">{ zombie.name }</CardTitle>
-            <CardDescription className="line-clamp-3">{ zombie.description }</CardDescription>
-          </CardContent>
+            <div className="space-y-2">
+              <CardTitle className="group-hover:text-primary-gradient group-focus-visible:text-primary-gradient">{ zombie.name }</CardTitle>
+              <CardDescription className="text-foreground/85">{ zombie.description }</CardDescription>
+            </div>
+          </CardHeader>
         </Card>
       </CustomLink>
     </article>
