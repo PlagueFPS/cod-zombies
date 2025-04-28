ba24dd5e6f8d94f2a89a69a6616b30702ce732bb
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-04-28 05:24:19 -0400

    update: `robots.ts`

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/unsubscribe'
     },
     sitemap: `${env.NEXT_PUBLIC_WEBSITE_URL}/sitemap.xml`
   }
