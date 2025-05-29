ef58bb7b34b41fe65c2daae30f837b4a246c2fd4
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-05-29 09:50:45 -0400

    feat: add 'the-tomb' map configuration to map registry

--- mapConfigs/index.ts
+++ mapConfigs/index.ts
@@ -1,5 +1,6 @@
 export const mapRegistry = {
   'shattered-veil': () => import('./shattered-veil').then((module) => module.default),
+  'the-tomb': () => import('./the-tomb').then((module) => module.default),
 } as const
 
 export type MapId = keyof typeof mapRegistry
---
+++ mapConfigs/the-tomb.ts
@@ -0,0 +1,10 @@
+import { MapConfig } from "@/types/InteractiveMap";
+
+const theTomb: MapConfig = {
+  id: "the-tomb",
+  title: "The Tomb",
+  image: "/layers/the-tomb.webp",
+  markers: []
+}
+
+export default theTomb
