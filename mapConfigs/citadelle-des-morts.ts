69aba5df47dec8840f48e4262ad165627113e6c7
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-05-30 10:17:17 -0400

    feat: Add 'citadelle-des-morts' map configuration to map registry

---
+++ mapConfigs/citadelle-des-morts.ts
@@ -0,0 +1,10 @@
+import type { MapConfig } from "@/types/InteractiveMap";
+
+const citadelleDesMorts: MapConfig = {
+  id: "citadelle-des-morts",
+  title: "Citadelle Des Morts",
+  image: "/layers/citadelle-des-morts.webp",
+  markers: []
+}
+
+export default citadelleDesMorts
--- mapConfigs/index.ts
+++ mapConfigs/index.ts
@@ -1,6 +1,7 @@
 export const mapRegistry = {
   'shattered-veil': () => import('./shattered-veil').then((module) => module.default),
   'the-tomb': () => import('./the-tomb').then((module) => module.default),
+  'citadelle-des-morts': () => import('./citadelle-des-morts').then((module) => module.default)
 } as const
 
 export type MapId = keyof typeof mapRegistry
