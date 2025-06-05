8f3c6efeae59ef966f699ea8a33c637d10a3df5f
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-06-05 16:18:12 -0400

    refactor: Update sitemap generation to include type annotations and ensure consistent lastModified date handling

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,30 +24,33 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}`,
       lastModified: new Date(maps[0].updatedAt),
     },
-    ...maps.filter(map => !map.isComingSoon).map(map => ({
+    ...maps.filter(map => !map.isComingSoon).map((map): MetadataRoute.Sitemap[number] => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${map.game.slug}/${map.slug}`,
-      lastModified: new Date(map.updatedAt)
+      lastModified: new Date(map.updatedAt),
     })),
     {
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests`,
-      lastModified: new Date(quests[0].updatedAt)
+      lastModified: new Date(quests[0].updatedAt),
     },
-    ...quests.map(q => ({
+    ...quests.map((q): MetadataRoute.Sitemap[number] => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/side-quests/${q.game.slug}/${q.map.slug}/${q.slug}`,
       lastModified: new Date(q.updatedAt),
     })),
     {
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/bestiary`,
-      lastModified: new Date(zombies[0].updatedAt)
+      lastModified: new Date(zombies[0].updatedAt),
     },
-    ...zombies.filter(z => !z.isComingSoon).map(z => ({
+    ...zombies.filter(z => !z.isComingSoon).map((z): MetadataRoute.Sitemap[number] => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/bestiary/${z.slug}`,
-      lastModified: new Date(z.updatedAt)
+      lastModified: new Date(z.updatedAt),
     })),
-    ...interactiveMaps.map(map => ({
+    {
+      url: `${env.NEXT_PUBLIC_WEBSITE_URL}/maps`,
+    },
+    ...interactiveMaps.map((map): MetadataRoute.Sitemap[number] => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/maps/${map}`,
     })),
-    ...legalDocs.map(doc => ({
+    ...legalDocs.map((doc): MetadataRoute.Sitemap[number] => ({
       url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${doc.slug}`,
       lastModified: new Date(doc.updatedAt),
     }))
