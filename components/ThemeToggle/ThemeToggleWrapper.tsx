d0f3742c71018c6eb0c9023e98fc0352c009c3b9
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-06-06 15:37:43 -0400

    feat: Integrate ThemeToggleLoader for improved loading experience in ThemeToggleWrapper

--- components/ThemeToggle/ThemeToggleWrapper.tsx
+++ components/ThemeToggle/ThemeToggleWrapper.tsx
@@ -1,9 +1,11 @@
 "use client"
 import dynamic from 'next/dynamic'
+import ThemeToggleLoader from '../Loaders/ThemeToggleLoader'
 
 // dynamic import to avoid hydration error for theme based styles
 const ThemeToggle = dynamic(() => import('@/components/ThemeToggle/ThemeToggle'), {
   ssr: false,
+  loading: () => <ThemeToggleLoader />
 })
 
 export default function ThemeToggleWrapper({ className }: { className?: string }) {
