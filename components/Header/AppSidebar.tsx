243b8b9bf3f45e324351996c10c3d6fb7a61dedc
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-06-06 16:13:50 -0400

    refactor: Update AppSidebar to use NavLink for navigation and restructure footer layout for improved responsiveness

--- components/Header/AppSidebar.tsx
+++ components/Header/AppSidebar.tsx
@@ -11,6 +11,7 @@ import X from "@/SVGs/XSVG";
 import Logo from "@/public/logo.webp"
 import Image from "next/image";
 import ThemeToggleWrapper from "../ThemeToggle/ThemeToggleWrapper";
+import NavLink from "../NavLink/NavLink";
 
 export default function AppSidebar() {
   return (
@@ -22,7 +23,7 @@ export default function AppSidebar() {
       </SheetTrigger>
       <SheetContent className="flex flex-col z-900">
         <SheetTitle className="sr-only">Sidebar</SheetTitle>
-        <SheetHeader className="border-b w-full pb-4 mt-6">
+        <SheetHeader className="border-b w-full pb-4">
           <SheetClose asChild>
             <CustomLink href={'/'} aria-label="Go to Home Page" className="flex items-center justify-center gap-2">
               <Image 
@@ -40,34 +41,35 @@ export default function AppSidebar() {
             </CustomLink>
           </SheetClose>
         </SheetHeader>
-        <nav className="flex flex-col items-start gap-6 pl-4 text-muted-foreground text-lg pb-4 w-full">
+        <nav className="flex flex-col items-start gap-6 pl-4 text-lg pb-4 w-full">
           { ROUTES.map(route => (
             <SheetClose key={ route.id } asChild>
-              <CustomLink 
+              <NavLink
                 href={ route.href } 
                 aria-label={`Go to ${route.title} page`} 
-                className='flex items-center justify-center gap-2 hover:text-foreground transition-all'
+                className='flex items-center justify-center gap-2 transition-all'
               >
                 <route.icon className='size-5 text-orange-400 dark:text-orange-200' />
                 <span className="font-medium">{ route.title }</span>
-              </CustomLink>
+              </NavLink>
             </SheetClose>
           ))}
         </nav>
-        <SheetFooter className="flex flex-row justify-evenly items-center w-full gap-3 text-muted-foreground mt-auto mb-4">
+        <SheetFooter className="flex flex-col justify-center items-center w-full gap-4 text-muted-foreground mt-auto mb-4 border-t">
           <ThemeToggleWrapper />
-          <Separator orientation="vertical" className="min-h-5" />
-          <ExternalLink href="https://x.com/CodZombiesGuide" title="Twitter" aria-label="Check out our Twitter profile">
-            <X className="size-5" />
-          </ExternalLink>
-          <Separator orientation="vertical" className="min-h-5" />
-          <ExternalLink href="https://discord.gg/callofduty" title="Discord" aria-label="Join the Official Call of Duty Discord">
-            <Discord className="size-5" />
-          </ExternalLink>
-          <Separator orientation="vertical" className="min-h-5" />
-          <ExternalLink href="https://www.reddit.com/r/CODZombies/" title="Reddit" aria-label="Join the Official Call of Duty: Zombies Subreddit">
-            <Reddit className="size-5" />
-          </ExternalLink>
+          <div className="flex justify-evenly items-center gap-3 w-full">
+            <ExternalLink href="https://x.com/CodZombiesGuide" title="Twitter" aria-label="Check out our Twitter profile">
+              <X className="size-5" />
+            </ExternalLink>
+            <Separator orientation="vertical" className="min-h-5" />
+            <ExternalLink href="https://discord.gg/callofduty" title="Discord" aria-label="Join the Official Call of Duty Discord">
+              <Discord className="size-5" />
+            </ExternalLink>
+            <Separator orientation="vertical" className="min-h-5" />
+            <ExternalLink href="https://www.reddit.com/r/CODZombies/" title="Reddit" aria-label="Join the Official Call of Duty: Zombies Subreddit">
+              <Reddit className="size-5" />
+            </ExternalLink>
+          </div>
         </SheetFooter>
       </SheetContent>
     </Sheet>
