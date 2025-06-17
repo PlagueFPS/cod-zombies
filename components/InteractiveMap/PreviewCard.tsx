9ebe56e3913007a4667e5f0d7b9a2846bf1f5df5
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-06-17 02:40:50 -0400

    fix: popin for `PreviewCard` images

--- components/InteractiveMap/PreviewCard.tsx
+++ components/InteractiveMap/PreviewCard.tsx
@@ -1,8 +1,8 @@
 import type { MapId } from "@/map-configs";
 import { getMapConfig } from "@/data/interactive-map";
-import Image from "next/image";
 import { Badge } from "../ui/badge";
 import { CustomLink } from "../CustomLink/CustomLink";
+import PreviewCardImage from "./PreviewCardImage";
 
 interface IPreviewCard {
   mapId: MapId
@@ -20,20 +20,12 @@ export default async function PreviewCard({ mapId, index }: IPreviewCard) {
       className="group"
     >
       <div className="flex flex-col items-start justify-center gap-4">
-        <div className="flex justify-center items-center w-full overflow-hidden rounded-md">
-          <Image 
-            unoptimized
-            src={`/previews/${map.id}-preview.webp`}
-            width={ 640 }
-            height={ 360 }
-            alt={ `${map.title} Preview Image` }
-            priority={ index === 0 }
-            className="h-full w-full object-cover group-hover:scale-105 transition-all duration-300"
-          />
+        <div className="flex justify-center items-center w-full overflow-hidden rounded-md shadow-xl dark:shadow-none">
+          <PreviewCardImage mapId={ mapId } title={ map.title } priority={ index === 0 } />
         </div>
         <div className="flex flex-col items-start justify-center">
           <Badge className="badge-primary-gradient dark:dark-badge-primary-gradient">{ map.game }</Badge>
-          <h3 className="text-xl font-bold group-hover:text-orange-800 group-hover:dark:text-orange-400 transition-colors">{ map.title }</h3>
+          <h3 className="text-xl font-bold group-hover:text-orange-600 group-hover:dark:text-orange-400 transition-colors">{ map.title }</h3>
         </div>
       </div>
     </CustomLink>
