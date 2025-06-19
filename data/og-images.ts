604e7ca6547c50485317126339ca92c5098d2669
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-06-19 15:50:22 -0400

    fix: production error: `no such file or directory` for fonts

--- data/og-images.ts
+++ data/og-images.ts
@@ -2,16 +2,16 @@ import 'server-only'
 import { FetchError } from '@/types/Error'
 import { tryCatch } from '@/utils/functions'
 import { err, ok, Result } from 'neverthrow'
-import { readFile } from 'node:fs/promises'
-import { join } from 'node:path'
+import { env } from '@/env'
 
 type AllowedFonts = "Geist-Bold.otf" | "Geist-SemiBold.otf"
 
-export const getFontData = async (font: AllowedFonts): Promise<Result<Buffer<ArrayBufferLike>, FetchError>> => {
-  const { data, error } = await tryCatch(readFile(join(process.cwd(), `fonts/${font}`)))
+export const getFontData = async (font: AllowedFonts): Promise<Result<ArrayBuffer, FetchError>> => {
+  const { data, error } = await tryCatch(fetch(`${env.NEXT_PUBLIC_WEBSITE_URL}/fonts/${font}`))
   if (error) {
     return err(new FetchError(error.message, { cause: error }))
   }
 
-  return ok(data)
+  const buffer = await data.arrayBuffer()
+  return ok(buffer)
 }
