59f368fdd5e38574c186658c9b61f18e900011ea
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-06-30 18:41:58 -0400

    chore: remove `botid` since it is currently flagging real users as bots

--- lib/action-helpers.ts
+++ lib/action-helpers.ts
@@ -1,6 +1,5 @@
 import "server-only"
 import { Schema } from "effect"
-import { checkBotId } from "botid/server"
 
 type ActionFunction<S extends Schema.Schema.AnyNoContext, T> = (
   data: Schema.Schema.Type<S>
@@ -8,8 +7,6 @@ type ActionFunction<S extends Schema.Schema.AnyNoContext, T> = (
 
 export const createAction = <S extends Schema.Schema.AnyNoContext, T>(schema: S, action: ActionFunction<S, T>) => {
   return async (_prevState: unknown, formData: FormData | Schema.Schema.Type<S>) => {
-    const { isBot } = await checkBotId()
-    if (isBot) return { success: false, message: "Bots are not allowed to perform this action." }
 
     const decodeFormData = Schema.decodeUnknownEither(schema)
     if (formData instanceof FormData) {
