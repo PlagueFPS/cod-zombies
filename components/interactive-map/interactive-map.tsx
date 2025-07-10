112a932a3880d8dda53f9211c10b8d9973ea0634
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-07-10 08:44:11 -0400

    feat: enable `experiemental.browserDebugInTerminal` flag for easier debugging

--- components/interactive-map/interactive-map.tsx
+++ components/interactive-map/interactive-map.tsx
@@ -1,8 +1,15 @@
 "use client"
 import "leaflet/dist/leaflet.css"
 import type { MapConfig } from "@/map-configs"
-import type { Location } from "@/map-configs/markers"
-import { CRS, LatLng, LatLngBounds, type LatLngTuple, type Map as LeafletMap, type LeafletMouseEvent } from "leaflet"
+import type { Location, MapMarker } from "@/map-configs/markers"
+import {
+	CRS,
+	LatLng,
+	LatLngBounds,
+	type LatLngTuple,
+	type Map as LeafletMap,
+	type LeafletMouseEvent,
+} from "leaflet"
 import { RotateCcw, ZoomIn, ZoomOut } from "lucide-react"
 import NextImage from "next/image"
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
@@ -29,14 +36,6 @@ export interface MapController {
 
 interface IInteractiveMap {
 	mapConfig: MapConfig
-}
-
-const logClickCoordinates = (imageDimensions: ImageDimensions | null) => (e: LeafletMouseEvent) => {
-	if (!IN_DEVELOPMENT || !e.latlng || !imageDimensions) return
-
-	const x = e.latlng.lng / imageDimensions.width
-	const y = 1 - e.latlng.lat / imageDimensions.height // Flip y back to normal
-	console.log(`Clicked coordinates: x: ${x.toFixed(3)}, y: ${y.toFixed(3)}`)
 }
 
 export default function InteractiveMap({ mapConfig }: IInteractiveMap) {
@@ -80,7 +79,10 @@ export default function InteractiveMap({ mapConfig }: IInteractiveMap) {
 	const convertToLeafletCoords = useCallback(
 		({ x, y }: Location): LatLng => {
 			if (!imageDimensions) return new LatLng(0, 0)
-			return new LatLng(imageDimensions.height - y * imageDimensions.height, x * imageDimensions.width)
+			return new LatLng(
+				imageDimensions.height - y * imageDimensions.height,
+				x * imageDimensions.width,
+			)
 		},
 		[imageDimensions],
 	)
@@ -103,7 +105,9 @@ export default function InteractiveMap({ mapConfig }: IInteractiveMap) {
 		<MapContainer
 			key={mapConfig.id}
 			ref={mapRef}
-			center={imageDimensions ? [imageDimensions.height / 2, imageDimensions.width / 2] : [1024, 1024]}
+			center={
+				imageDimensions ? [imageDimensions.height / 2, imageDimensions.width / 2] : [1024, 1024]
+			}
 			zoom={0}
 			minZoom={-2}
 			maxZoom={3}
@@ -114,7 +118,9 @@ export default function InteractiveMap({ mapConfig }: IInteractiveMap) {
 			className="relative bg-accent! dark:bg-secondary-alternative!"
 		>
 			<MapController imageDimensions={imageDimensions} />
-			{imageDimensions && <ImageOverlay key={mapConfig.id} url={mapConfig.image} bounds={getImageBounds()} />}
+			{imageDimensions && (
+				<ImageOverlay key={mapConfig.id} url={mapConfig.image} bounds={getImageBounds()} />
+			)}
 			{/* We do not map through filteredMarkers for rendering to avoid icon flickering */}
 			{imageDimensions &&
 				mapConfig.markers.map(marker => {
@@ -136,39 +142,21 @@ export default function InteractiveMap({ mapConfig }: IInteractiveMap) {
 							position={convertToLeafletCoords(location)}
 						>
 							{marker.type !== "label" ? (
-								<Popup className="custom-popup">
-									<div className="absolute top-4 left-4 mb-1 flex w-full items-center gap-2">
-										<MarkerBadge category={marker.category}>{capitalize(marker.category)}</MarkerBadge>
-									</div>
-									{marker.icon && (
-										<div className="flex w-full items-center justify-center">
-											<NextImage
-												unoptimized
-												src={marker.icon}
-												alt={marker.title}
-												width={128}
-												height={128}
-												className={cn(
-													"size-16",
-													{ "size-12": marker.type === "perk" && marker.id !== "der-wunderfizz" },
-													{ "size-12": marker.id === "dark-aether-lantern" },
-												)}
-											/>
-										</div>
-									)}
-									<h3 className="dark:dark-text-gradient text-center font-extrabold text-gradient text-lg">
-										{location.title || marker.title}
-									</h3>
-									<p className="px-2 text-center text-foreground/90 text-sm">
-										{location.description || marker.description}
-									</p>
-								</Popup>
+								<CustomPopup marker={ marker } location={ location } />
 							) : null}
 						</CustomMarker>
 					))
 				})}
 		</MapContainer>
 	)
+}
+
+const logClickCoordinates = (imageDimensions: ImageDimensions | null) => (e: LeafletMouseEvent) => {
+	if (!IN_DEVELOPMENT || !e.latlng || !imageDimensions) return
+
+	const x = e.latlng.lng / imageDimensions.width
+	const y = 1 - e.latlng.lat / imageDimensions.height // Flip y back to normal
+	console.log(`Clicked coordinates: x: ${x.toFixed(3)}, y: ${y.toFixed(3)}`)
 }
 
 function MapController({ imageDimensions }: MapController) {
@@ -198,8 +186,9 @@ function MapController({ imageDimensions }: MapController) {
 		}
 	}
 
+
 	return (
-		<div className="fixed top-18 right-4 z-500 flex gap-2">
+		<div className="fixed top-20 right-8 z-500 flex gap-2">
 			<Badge variant={"outline"} className="rounded-md bg-background/80">
 				<div className="flex flex-col gap-1">
 					<Button variant={"ghost"} size={"icon"} onClick={handleZoomIn} title="Zoom In">
@@ -216,5 +205,39 @@ function MapController({ imageDimensions }: MapController) {
 				</div>
 			</Badge>
 		</div>
+	)
+}
+
+function CustomPopup({ marker, location }: { marker: MapMarker; location: Location }) {
+	return (
+		<Popup
+			className="custom-popup"
+		>
+			<div className="absolute top-4 left-4 mb-1 flex w-full items-center gap-2">
+				<MarkerBadge category={marker.category}>{capitalize(marker.category)}</MarkerBadge>
+			</div>
+			{marker.icon && (
+				<div className="flex w-full items-center justify-center">
+					<NextImage
+						unoptimized
+						src={marker.icon}
+						alt={marker.title}
+						width={128}
+						height={128}
+						className={cn(
+							"size-16",
+							{ "size-12": marker.type === "perk" && marker.id !== "der-wunderfizz" },
+							{ "size-12": marker.id === "dark-aether-lantern" },
+						)}
+					/>
+				</div>
+			)}
+			<h3 className="dark:dark-text-gradient text-center font-extrabold text-gradient text-lg">
+				{location.title || marker.title}
+			</h3>
+			<p className="px-2 text-center text-foreground/90 text-sm">
+				{location.description || marker.description}
+			</p>
+		</Popup>
 	)
 }
--- next.config.ts
+++ next.config.ts
@@ -1,91 +1,94 @@
 import "./env"
-import type { NextConfig } from "next";
-import { withBotId } from "botid/next/config";
+import type { NextConfig } from "next"
+import { withBotId } from "botid/next/config"
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.0.0.*"],
-  logging: {
-    fetches: {
-      hmrRefreshes: true,
-      fullUrl: true,
-    }
-  },
-  experimental: {
-    ppr: true,
-    serverComponentsHmrCache: true,
-    reactCompiler: true,
-  },
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: `images.ctfassets.net`,
-        pathname: `/${process.env.CONTENTFUL_SPACE_ID}/**`,
-      }
-    ],
-    minimumCacheTTL: 31536000, // 1 year in seconds
-  },
-  async redirects() {
-    return [
-      {
-        source: '/black-ops-1',
-        destination: '/?game=black-ops-1',
-        permanent: true,
-      },
-      {
-        source: '/black-ops-2',
-        destination: '/?game=black-ops-2',
-        permanent: true,
-      },
-      {
-        source: '/black-ops-3',
-        destination: '/?game=black-ops-3',
-        permanent: true,
-      },
-      {
-        source: '/black-ops-4',
-        destination: '/?game=black-ops-4',
-        permanent: true,
-      },
-      {
-        source: '/black-ops-cold-war',
-        destination: '/?game=black-ops-cold-war',
-        permanent: true,
-      },
-      {
-        source: '/black-ops-6',
-        destination: '/?game=black-ops-6',
-        permanent: true,
-      },
-      {
-        source: '/side-quests/black-ops-6',
-        destination: '/side-quests?game=black-ops-6',
-        permanent: true,
-      },
-      {
-        source: '/side-quests/black-ops-6/liberty-falls',
-        destination: '/side-quests?map=liberty-falls',
-        permanent: true,
-      },
-      {
-        source: '/side-quests/black-ops-6/terminus',
-        destination: '/side-quests?map=terminus',
-        permanent: true,
-      },
-      {
-        source: '/side-quests/black-ops-6/citadelle-des-morts',
-        destination: '/side-quests?map=citadelle-des-morts',
-        permanent: true,
-      },
-      {
-        source: '/side-quests/black-ops-6/the-tomb',
-        destination: '/side-quests?map=the-tomb',
-        permanent: true,
-      },
-    ]
-  },
-};
+	allowedDevOrigins: ["10.0.0.*"],
+	logging: {
+		fetches: {
+			hmrRefreshes: true,
+			fullUrl: true,
+		},
+	},
+	experimental: {
+		ppr: true,
+		serverComponentsHmrCache: true,
+		reactCompiler: true,
+		browserDebugInfoInTerminal: {
+			showSourceLocation: true,
+		},
+	},
+	images: {
+		formats: ["image/avif", "image/webp"],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: `images.ctfassets.net`,
+				pathname: `/${process.env.CONTENTFUL_SPACE_ID}/**`,
+			},
+		],
+		minimumCacheTTL: 31536000, // 1 year in seconds
+	},
+	async redirects() {
+		return [
+			{
+				source: "/black-ops-1",
+				destination: "/?game=black-ops-1",
+				permanent: true,
+			},
+			{
+				source: "/black-ops-2",
+				destination: "/?game=black-ops-2",
+				permanent: true,
+			},
+			{
+				source: "/black-ops-3",
+				destination: "/?game=black-ops-3",
+				permanent: true,
+			},
+			{
+				source: "/black-ops-4",
+				destination: "/?game=black-ops-4",
+				permanent: true,
+			},
+			{
+				source: "/black-ops-cold-war",
+				destination: "/?game=black-ops-cold-war",
+				permanent: true,
+			},
+			{
+				source: "/black-ops-6",
+				destination: "/?game=black-ops-6",
+				permanent: true,
+			},
+			{
+				source: "/side-quests/black-ops-6",
+				destination: "/side-quests?game=black-ops-6",
+				permanent: true,
+			},
+			{
+				source: "/side-quests/black-ops-6/liberty-falls",
+				destination: "/side-quests?map=liberty-falls",
+				permanent: true,
+			},
+			{
+				source: "/side-quests/black-ops-6/terminus",
+				destination: "/side-quests?map=terminus",
+				permanent: true,
+			},
+			{
+				source: "/side-quests/black-ops-6/citadelle-des-morts",
+				destination: "/side-quests?map=citadelle-des-morts",
+				permanent: true,
+			},
+			{
+				source: "/side-quests/black-ops-6/the-tomb",
+				destination: "/side-quests?map=the-tomb",
+				permanent: true,
+			},
+		]
+	},
+}
 
-export default withBotId(nextConfig);
+export default withBotId(nextConfig)
