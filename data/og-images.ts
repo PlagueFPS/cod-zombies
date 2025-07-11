3816d6824411a810f518e2014a6885870ebbfc5e
Plague <74983533+PlagueFPS@users.noreply.github.com>
2025-07-11 10:42:57 -0400

    chore: move `@effect/platform-node` to `devDeps`

--- data/og-images.ts
+++ data/og-images.ts
@@ -26,17 +26,3 @@ export const getFontData = Effect.gen(function* () {
 	Effect.tapError(Effect.logError),
 	Effect.catchAll(() => Effect.succeed(null)),
 )
-
-// export const getFontData = async () => {
-// 	try {
-// 		const [geistSemiBold, geistBold] = await Promise.all([
-// 			readFile(join(process.cwd(), "assets/Geist-SemiBold.otf")),
-// 			readFile(join(process.cwd(), "assets/Geist-Bold.otf")),
-// 		])
-
-// 		return { geistSemiBold, geistBold }
-// 	} catch (error) {
-// 		console.error(new LoadFontDataError({ message: "Failed to load font data.", cause: error }))
-// 		return null
-// 	}
-// }
