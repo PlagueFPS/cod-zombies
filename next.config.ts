c957e5de67913e3a787ab19af0404ddd671ff1b9
Plague <pichardoangel416@gmail.com>
2025-07-18 01:18:11 -0400

    [images] only allow images qualities of `75` or `100`

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
 	images: {
 		formats: ["image/avif", "image/webp"],
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-		qualities: [75, 85, 100],
+		qualities: [75, 100],
 		remotePatterns: [
 			{
 				protocol: "https",
