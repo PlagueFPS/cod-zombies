f304fa9884d3cbb3fc68a6585faf7e1cef18da21
Plague <pichardoangel416@gmail.com>
2025-07-19 16:38:31 -0400

    feat(use-local-storage): add function to allow deleting the stored key

--- hooks/use-local-storage.ts
+++ hooks/use-local-storage.ts
@@ -2,16 +2,18 @@
 import { useCallback, useState } from "react"
 
 type SetValue<T> = (value: T | ((value: T) => T)) => void
+type LocalStorageResult<T> = [T, SetValue<T>, () => void]
 /**
  * Provides a way to store and retrieve values from the browser's local storage.
  *
  * @param key The key to store the value under in local storage.
  * @param initialValue The value to return if no value is set in local storage.
- * @returns A tuple of two values. The first value is the stored value, or the initial value if no value is set.
+ * @returns A tuple of three values. The first value is the stored value, or the initial value if no value is set.
  * The second value is a function that can be used to set the stored value.
+ * The third value is a function that can be used to delete the stored value.
  */
 
-export const useLocalStorage = <T>(key: string, initialValue: T): [T, SetValue<T>] => {
+export const useLocalStorage = <T>(key: string, initialValue: T): LocalStorageResult<T> => {
 	// Get from local storage then parse stored json or return initialValue
 	const readValue = useCallback((): T => {
 		// Prevent build error "window is undefined" but keep working
@@ -52,5 +54,16 @@ export const useLocalStorage = <T>(key: string, initialValue: T): [T, SetValue<T
 		[key, storedValue],
 	)
 
-	return [storedValue, setValue]
+	const deleteValue = useCallback(() => {
+		try {
+			if (typeof window !== "undefined") {
+				window.localStorage.removeItem(key)
+			}
+			setStoredValue(initialValue)
+		} catch (error) {
+			console.warn(`Error deleting localStorage key "${key}":`, error)
+		}
+	}, [key, initialValue])
+
+	return [storedValue, setValue, deleteValue]
 }
