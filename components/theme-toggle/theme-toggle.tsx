5b53feee76d4d78096a1a453936f4db79cec7413
Plague <pichardoangel416@gmail.com>
2025-07-20 13:04:09 -0400

    feat(theme-toggle): add keyboard shortcut `T` for toggling the theme

--- components/theme-toggle/theme-toggle.tsx
+++ components/theme-toggle/theme-toggle.tsx
@@ -2,7 +2,9 @@
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
+import { useKeyboardShortcut } from "@/hooks/use-keyboard-shortcut"
 import { cn } from "@/lib/utils"
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip"
 
 interface ThemeToggleProps {
 	className?: string
@@ -16,21 +18,39 @@ export default function ThemeToggle({ className }: ThemeToggleProps) {
 		else setTheme("light")
 	}
 
+	useKeyboardShortcut({
+		shortcut: "T",
+		callback: () => handleThemeToggle(),
+	})
+
 	return (
 		<div className="flex w-fit p-0.5">
-			<Button
-				variant="outline"
-				size="icon"
-				type="button"
-				aria-label="Toggle Theme"
-				title="Toggle Theme"
-				className={cn("size-8 cursor-pointer bg-transparent text-foreground", className, {
-					"bg-accent text-foreground": theme === "light",
-				})}
-				onClick={handleThemeToggle}
-			>
-				{theme === "dark" ? <Sun className="size-4 transition-all" /> : <Moon className="size-4 transition-all" />}
-			</Button>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<Button
+						variant="outline"
+						size="icon"
+						type="button"
+						aria-label="Toggle Theme; keyboard shorcut is T"
+						className={cn("size-8 cursor-pointer bg-transparent text-foreground", className, {
+							"bg-accent text-foreground": theme === "light",
+						})}
+						onClick={handleThemeToggle}
+					>
+						{theme === "dark" ? (
+							<Sun className="size-4 transition-all" />
+						) : (
+							<Moon className="size-4 transition-all" />
+						)}
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent sideOffset={5} className="z-999 flex items-center justify-center gap-2">
+					<span>Toggle Theme</span>
+					<kbd className="pointer-events-none ml-auto inline-flex h-5 select-none items-center gap-1 rounded bg-muted px-1.5 font-medium text-muted-foreground text-xs opacity-100">
+						T
+					</kbd>
+				</TooltipContent>
+			</Tooltip>
 		</div>
 	)
 }
