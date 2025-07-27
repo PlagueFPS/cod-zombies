7b1d5580affe7e601d5f4b9b9dc3556a3d263a35
Plague <pichardoangel416@gmail.com>
2025-07-27 03:00:18 -0400

    fix: import paths

--- app/(main)/(Home)/page.tsx
+++ app/(main)/(Home)/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react"
 import GridSection from "@/components/grid-section/grid-section"
 import HeroSection from "@/components/hero-section/hero-section"
 import GridLoader from "@/components/loaders/grid-loader"
-import MapFiltersLoader from "@/components/loaders/map-filters-loader"
+import QuestFilterLoader from "@/components/loaders/quest-filter-loader"
 import { MainQuestFilters } from "@/components/quest-filters/quest-filters"
 import { MainQuestGrid } from "@/components/quest-grid/quest-grid"
 import { env } from "@/env"
@@ -19,7 +19,7 @@ export default function Home() {
 		<div className="container flex flex-col items-center justify-center gap-12">
 			<HeroSection />
 			<GridSection title="Main Quests">
-				<Suspense fallback={<MapFiltersLoader />}>
+				<Suspense fallback={<QuestFilterLoader filters={["Game", "Difficulty"]} />}>
 					<MainQuestFilters />
 				</Suspense>
 				<Suspense fallback={<GridLoader />}>
--- components/loaders/home-page-loader.tsx
+++ components/loaders/home-page-loader.tsx
@@ -2,14 +2,14 @@ import GridSection from "@/components/grid-section/grid-section"
 import HeroSection from "@/components/hero-section/hero-section"
 import GridLoader from "@/components/loaders/grid-loader"
 import GridPaginationLoader from "@/components/loaders/grid-pagination-loader"
-import MapFiltersLoader from "@/components/loaders/map-filters-loader"
+import QuestFilterLoader from "@/components/loaders/quest-filter-loader"
 
 export default function HomeLoader() {
 	return (
 		<div className="container flex flex-col items-center justify-center gap-12">
 			<HeroSection />
 			<GridSection title="Main Quests">
-				<MapFiltersLoader />
+				<QuestFilterLoader filters={["Game", "Difficulty"]} />
 				<GridLoader />
 				<GridPaginationLoader />
 			</GridSection>
