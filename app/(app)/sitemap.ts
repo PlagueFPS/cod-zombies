3bc2e64345c7f16e2c847ea15c5ab4e025b21de1
Plague <pichardoangel416@gmail.com>
2025-09-07 00:17:49 -0400

    fix: wrong properties being accessed within sitemap

--- app/(app)/sitemap.ts
+++ app/(app)/sitemap.ts
@@ -4,17 +4,20 @@ import { getMainQuestMetadata } from "@/data/main-quests"
 import { getSideQuestsMetadata } from "@/data/side-quests"
 import { getZombiesMetadata } from "@/data/zombies"
 import { env } from "@/env"
+import { getLegalDocsMetadata } from "@/data/legal"
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const mainQuestsPromise = getMainQuestMetadata()
 	const zombiesPromise = getZombiesMetadata()
 	const sideQuestsPromise = getSideQuestsMetadata()
+	const legalDocsPromise = getLegalDocsMetadata()
 	const interactiveMaps = getAvailableMaps()
 
-	const [mainQuests, zombies, sideQuests] = await Promise.all([
+	const [mainQuests, zombies, sideQuests, legalDocs] = await Promise.all([
 		mainQuestsPromise,
 		zombiesPromise,
 		sideQuestsPromise,
+		legalDocsPromise,
 	])
 
 	return [
@@ -23,7 +26,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			lastModified: mainQuests[0] ? new Date(mainQuests[0].updatedAt) : undefined,
 		},
 		...mainQuests.map((quest): MetadataRoute.Sitemap[number] => ({
-			url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${quest.game}/${quest.slug}`,
+			url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${quest.game.slug}/${quest.slug}`,
 			lastModified: new Date(quest.updatedAt),
 		})),
 		{
@@ -48,9 +51,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 		...interactiveMaps.map((map): MetadataRoute.Sitemap[number] => ({
 			url: `${env.NEXT_PUBLIC_WEBSITE_URL}/maps/${map}`,
 		})),
-		// ...legalDocs.map((doc): MetadataRoute.Sitemap[number] => ({
-		// 	url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${doc.slug}`,
-		// 	lastModified: new Date(doc.updatedAt),
-		// })),
+		...legalDocs.map((doc): MetadataRoute.Sitemap[number] => ({
+			url: `${env.NEXT_PUBLIC_WEBSITE_URL}/${doc.slug}`,
+			lastModified: new Date(doc.updatedAt),
+		})),
 	]
 }
