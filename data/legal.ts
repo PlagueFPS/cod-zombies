aa614b8adc2f1e53071f26e2bd9b3201abf12453
Plague <pichardoangel416@gmail.com>
2025-09-07 00:17:27 -0400

    feat(DAL): add legal docs data fetching

--- data/legal.ts
+++ data/legal.ts
@@ -2,7 +2,7 @@ import { Effect } from "effect"
 import { unstable_cache } from "next/cache"
 import { cache } from "react"
 import { Payload } from "@/lib/services/Payload"
-import { EntryNotFoundError } from "@/types/errors"
+import { EntryNotFoundError, GetEntriesError } from "@/types/errors"
 import { CACHE_KEYS, IN_DEVELOPMENT } from "@/utils/constants"
 
 export const getLegalDocBySlug = cache(
@@ -48,3 +48,35 @@ export const getLegalDocBySlug = cache(
 		},
 	),
 )
+
+export const getLegalDocsMetadata = cache(unstable_cache(async () => {
+	return await Effect.gen(function*(){
+		const payload = yield* Payload
+		const legalDocs = yield* Effect.tryPromise({
+			try: () => payload.find({
+				collection: "legal",
+				pagination: false,
+				draft: IN_DEVELOPMENT,
+				select: {
+					slug: true,
+					updatedAt: true,
+				},
+			}),
+			catch: error => new GetEntriesError({
+				message: "Failed to get legal docs metadata",
+				cause: error,
+			})
+		}).pipe(Effect.map(legalDocs => legalDocs.docs))
+
+		return legalDocs
+	}).pipe(
+		Effect.withLogSpan("get_legal_docs_metadata"),
+		Effect.tapError(Effect.logError),
+		Effect.catchAll(_error => Effect.succeed([])),
+		Effect.ensureErrorType<never>(),
+		Effect.provide(Payload.Default),
+		Effect.runPromise,
+	)
+}, [], {
+	tags: [CACHE_KEYS.legal.all]
+}))
