6bc2cb5c29751353d610a86fc2775bf256137c02
Plague <pichardoangel416@gmail.com>
2025-09-17 03:50:20 -0400

    refactor: remove `customImageLoader` as it is no longer needed

--- components/icon-image/icon-image.tsx
+++ components/icon-image/icon-image.tsx
@@ -3,7 +3,6 @@ import type { ImageProps } from "@/types/images"
 import Image from "next/image"
 import { useImageState } from "@/hooks/use-image-state"
 import { cn } from "@/lib/utils"
-import { customImageLoader } from "@/utils/image-loader"
 
 interface IconImageProps extends ImageProps {
 	withLoader?: boolean
@@ -18,16 +17,7 @@ export default function IconImage({
 	sizes,
 	withLoader = false,
 }: IconImageProps) {
-	const {
-		imageLoaded,
-		imageErrored,
-		fallbackLoaded,
-		fallbackErrored,
-		setImageLoaded,
-		setImageErrored,
-		setFallbackLoaded,
-		setFallbackErrored,
-	} = useImageState()
+	const { imageLoaded, imageErrored, setImageLoaded, setImageErrored } = useImageState()
 	const featuredImageURL = featuredImage?.url ? featuredImage.url : null
 
 	if (!featuredImageURL) return null
@@ -53,24 +43,6 @@ export default function IconImage({
 						"animate-fade-in opacity-100": imageLoaded,
 					})}
 					priority={priority}
-				/>
-			) : null}
-			{imageErrored && !fallbackErrored ? (
-				<Image
-					src={featuredImageURL}
-					loader={({ src, width, quality }) => customImageLoader({ src, width, quality })}
-					alt={alt}
-					width={featuredImage?.width ?? undefined}
-					height={featuredImage?.height ?? undefined}
-					sizes={sizes}
-					onLoad={() => setFallbackLoaded(true)}
-					onError={() => setFallbackErrored(true)}
-					quality={quality}
-					className={cn("flex h-auto w-full items-center justify-center opacity-0", className, {
-						"animate-fade-in opacity-100": fallbackLoaded,
-					})}
-					priority={priority}
-					placeholder={!featuredImage ? "blur" : undefined}
 				/>
 			) : null}
 		</>
