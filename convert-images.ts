f09b63caeb5bf1d473eee9a83f787f62140e7297
Plague <pichardoangel416@gmail.com>
2025-09-25 11:12:44 -0400

    feat: populate casimir mechanism main quest MDX content

---
+++ convert-images.ts
@@ -0,0 +1,44 @@
+import { FileSystem, Path } from "@effect/platform"
+import { NodeFileSystem, NodeRuntime } from "@effect/platform-node"
+import { Effect, Layer, Ref } from "effect"
+import sharp from "sharp"
+
+const program = Effect.gen(function* () {
+	const fs = yield* FileSystem.FileSystem
+	const path = yield* Path.Path
+	const media = yield* fs.readDirectory("./media")
+	const numRef = yield* Ref.make(0)
+
+	yield* Effect.forEach(media, file =>
+		Effect.gen(function* () {
+			if (!file.startsWith("ascension-")) return
+
+			const image = yield* fs.readFile(path.join("./media", file))
+			const extension = path.extname(file)
+			if (extension === ".webp") return
+
+			const optimizedImage = yield* Effect.tryPromise({
+				try: () => sharp(image).webp({ effort: 6 }).toBuffer(),
+				catch: error => new Error(`Failed to transform image: ${file}`, { cause: error }),
+			})
+
+			yield* fs.writeFile(
+				path.join(
+					process.cwd(),
+					"./content/main-quests/images/ascension",
+					file.replace(extension, ".webp"),
+				),
+				optimizedImage,
+			)
+			yield* Effect.log(`Converted ${file} to webp`)
+			yield* Ref.update(numRef, n => n + 1)
+		}),
+	)
+	const current = yield* Ref.get(numRef)
+	yield* Effect.log(`Converted ${current} images`)
+}).pipe(
+	Effect.withLogSpan("convert_images"),
+	Effect.provide(Layer.merge(NodeFileSystem.layer, Path.layer)),
+)
+
+NodeRuntime.runMain(program)
