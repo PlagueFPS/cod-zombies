2ec82b6768dffbdfacce941aee3ed83565bed582
Plague <pichardoangel416@gmail.com>
2025-09-26 13:44:32 -0400

    feat: populated Little Lost Girl main quest MDX content

--- convert-images.ts
+++ convert-images.ts
@@ -12,7 +12,7 @@ const program = Effect.gen(function* () {
 
 	yield* Effect.forEach(media, file =>
 		Effect.gen(function* () {
-			if (!file.startsWith("buried-")) return
+			if (!file.startsWith("origins-")) return
 
 			let image = yield* fs.readFile(path.join("./media", file))
 			const extension = path.extname(file)
@@ -26,10 +26,10 @@ const program = Effect.gen(function* () {
 			}
 
 			yield* fs.writeFile(
-				path.join(process.cwd(), "./content/images/buried", file.replace(extension, ".webp")),
+				path.join(process.cwd(), "./content/images/origins", file.replace(extension, ".webp")),
 				image,
 			)
-			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to buried`)
+			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to origins`)
 			yield* Ref.update(numRef, n => n + 1)
 		}),
 	)
