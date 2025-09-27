da077307074b2f28eed53a45e8cadd11e1248653
Plague <pichardoangel416@gmail.com>
2025-09-27 11:13:51 -0400

    feat: populate Seeds of Doubt main quest MDX content

--- convert-images.ts
+++ convert-images.ts
@@ -12,7 +12,7 @@ const program = Effect.gen(function* () {
 
 	yield* Effect.forEach(media, file =>
 		Effect.gen(function* () {
-			if (!file.startsWith("de-")) return
+			if (!file.startsWith("zns-")) return
 
 			let image = yield* fs.readFile(path.join("./media", file))
 			const extension = path.extname(file)
@@ -28,12 +28,12 @@ const program = Effect.gen(function* () {
 			yield* fs.writeFile(
 				path.join(
 					process.cwd(),
-					"./content/images/der-eisendrache",
+					"./content/images/zetsubou-no-shima",
 					file.replace(extension, ".webp"),
 				),
 				image,
 			)
-			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to der-eisendrache`)
+			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to zetsubou-no-shima`)
 			yield* Ref.update(numRef, n => n + 1)
 		}),
 	)
