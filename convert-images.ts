2dc5c082d90456fea74013696ef717915281a515
Plague <pichardoangel416@gmail.com>
2025-09-27 10:22:57 -0400

    feat: populated My Brother's Keeper main quest MDX content

--- convert-images.ts
+++ convert-images.ts
@@ -12,7 +12,7 @@ const program = Effect.gen(function* () {
 
 	yield* Effect.forEach(media, file =>
 		Effect.gen(function* () {
-			if (!file.startsWith("the-giant-")) return
+			if (!file.startsWith("de-")) return
 
 			let image = yield* fs.readFile(path.join("./media", file))
 			const extension = path.extname(file)
@@ -26,10 +26,14 @@ const program = Effect.gen(function* () {
 			}
 
 			yield* fs.writeFile(
-				path.join(process.cwd(), "./content/images/the-giant", file.replace(extension, ".webp")),
+				path.join(
+					process.cwd(),
+					"./content/images/der-eisendrache",
+					file.replace(extension, ".webp"),
+				),
 				image,
 			)
-			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to the-giant`)
+			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to der-eisendrache`)
 			yield* Ref.update(numRef, n => n + 1)
 		}),
 	)
