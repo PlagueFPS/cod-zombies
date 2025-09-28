b96ef6f4be3f673efd6a26acafa6528794d39df4
Plague <pichardoangel416@gmail.com>
2025-09-28 07:01:00 -0400

    feat: populated Abandon Ship main quest MDX content

--- convert-images.ts
+++ convert-images.ts
@@ -12,7 +12,7 @@ const program = Effect.gen(function* () {
 
 	yield* Effect.forEach(media, file =>
 		Effect.gen(function* () {
-			if (!file.startsWith("revelations-")) return
+			if (!file.startsWith("voyage-of-despair-")) return
 
 			let image = yield* fs.readFile(path.join("./media", file))
 			const extension = path.extname(file)
@@ -26,10 +26,14 @@ const program = Effect.gen(function* () {
 			}
 
 			yield* fs.writeFile(
-				path.join(process.cwd(), "./content/images/revelations", file.replace(extension, ".webp")),
+				path.join(
+					process.cwd(),
+					"./content/images/voyage-of-despair",
+					file.replace(extension, ".webp"),
+				),
 				image,
 			)
-			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to revelations`)
+			yield* Effect.log(`Optimized: ${shouldOptimize}; moved ${file} to voyage-of-despair`)
 			yield* Ref.update(numRef, n => n + 1)
 		}),
 	)
