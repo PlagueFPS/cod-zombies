2cbc7cf117dd58da88f3b02070b094bacd16b86b
Angel P. <pichardoangel416@gmail.com>
2025-10-13 14:54:08 -0400

    update: interactive map preview card `prefecth` prop to `auto`

--- components/interactive-map/preview-card.tsx
+++ components/interactive-map/preview-card.tsx
@@ -7,49 +7,50 @@ import { Badge } from "../ui/badge"
 import PreviewCardImage from "./preview-card-image"
 
 interface IPreviewCard {
-	mapId: MapId
-	index: number
+  mapId: MapId
+  index: number
 }
 
 export default async function PreviewCard({ mapId, index }: IPreviewCard) {
-	const config = await getMapConfig(mapId)
-	if (!config) return null
+  const config = await getMapConfig(mapId)
+  if (!config) return null
 
-	return (
-		<CustomLink
-			href={`/maps/${config.id}`}
-			aria-label={`View ${config.title} interactive map`}
-			className={cn("group outline-none", {
-				"pointer-events-none opacity-75 dark:opacity-50": config.state === "Coming Soon",
-			})}
-			aria-disabled={config.state === "Coming Soon"}
-			tabIndex={config.state === "Coming Soon" ? -1 : 0}
-		>
-			<div className="flex flex-col items-start justify-center gap-4">
-				<div className="flex w-full items-center justify-center overflow-hidden rounded-md shadow-xl group-focus-visible:outline-2 group-focus-visible:outline-primary dark:shadow-none">
-					<PreviewCardImage
-						mapId={mapId}
-						title={config.title}
-						priority={index === 0}
-						className="transition-transform duration-300 will-change-transform group-focus-visible:scale-105"
-					/>
-				</div>
-				<div className="flex flex-col items-start justify-center">
-					<div className="flex items-center gap-2">
-						{config.state === "Coming Soon" ? (
-							<ComingSoonBadge />
-						) : config.state === "New" ? (
-							<NewBadge />
-						) : null}
-						<Badge className="badge-primary-gradient dark:dark-badge-primary-gradient">
-							{config.game}
-						</Badge>
-					</div>
-					<h3 className="font-bold text-xl transition-colors group-hover:text-primary group-focus-visible:text-primary">
-						{config.title}
-					</h3>
-				</div>
-			</div>
-		</CustomLink>
-	)
+  return (
+    <CustomLink
+      prefetch="auto"
+      href={`/maps/${config.id}`}
+      aria-label={`View ${config.title} interactive map`}
+      className={cn("group outline-none", {
+        "pointer-events-none opacity-75 dark:opacity-50": config.state === "Coming Soon",
+      })}
+      aria-disabled={config.state === "Coming Soon"}
+      tabIndex={config.state === "Coming Soon" ? -1 : 0}
+    >
+      <div className="flex flex-col items-start justify-center gap-4">
+        <div className="flex w-full items-center justify-center overflow-hidden rounded-md shadow-xl group-focus-visible:outline-2 group-focus-visible:outline-primary dark:shadow-none">
+          <PreviewCardImage
+            mapId={mapId}
+            title={config.title}
+            priority={index === 0}
+            className="transition-transform duration-300 will-change-transform group-focus-visible:scale-105"
+          />
+        </div>
+        <div className="flex flex-col items-start justify-center">
+          <div className="flex items-center gap-2">
+            {config.state === "Coming Soon" ? (
+              <ComingSoonBadge />
+            ) : config.state === "New" ? (
+              <NewBadge />
+            ) : null}
+            <Badge className="badge-primary-gradient dark:dark-badge-primary-gradient">
+              {config.game}
+            </Badge>
+          </div>
+          <h3 className="font-bold text-xl transition-colors group-hover:text-primary group-focus-visible:text-primary">
+            {config.title}
+          </h3>
+        </div>
+      </div>
+    </CustomLink>
+  )
 }
