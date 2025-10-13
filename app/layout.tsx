ef4d0ad0b8b6f98724633a6dd97510ba83b85507
Angel P. <pichardoangel416@gmail.com>
2025-10-13 01:19:23 -0700

    feat: enable `@vercel/speed-insights`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,94 +1,101 @@
-import "./globals.css"
-import type { Metadata, Viewport } from "next"
-import { GoogleAnalytics } from "@next/third-parties/google"
-import { Analytics } from "@vercel/analytics/react"
-import { Geist, Geist_Mono } from "next/font/google"
-import { HashLinkHandler } from "@/components/custom-link/custom-link"
-import Header from "@/components/header/header"
-import ReactScanWrapper from "@/components/react-scan/react-scan-wrapper"
-import { Toaster } from "@/components/ui/sonner"
-import { KeyboardShortcutsProvider } from "@/contexts/keyboard-shortcuts"
-import { ThemeProvider } from "@/contexts/theme-provider"
-import { GLOBAL_OG_PROPS, IN_DEVELOPMENT, SITE_DESCRIPTION, SITE_TITLE } from "@/utils/constants"
-import { getServerUrl } from "@/utils/functions"
+import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { HashLinkHandler } from "@/components/custom-link/custom-link";
+import Header from "@/components/header/header";
+import ReactScanWrapper from "@/components/react-scan/react-scan-wrapper";
+import { Toaster } from "@/components/ui/sonner";
+import { KeyboardShortcutsProvider } from "@/contexts/keyboard-shortcuts";
+import { ThemeProvider } from "@/contexts/theme-provider";
+import {
+  GLOBAL_OG_PROPS,
+  IN_DEVELOPMENT,
+  SITE_DESCRIPTION,
+  SITE_TITLE,
+} from "@/utils/constants";
+import { getServerUrl } from "@/utils/functions";
 
 interface LayoutProps {
-	children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const metadata: Metadata = {
-	metadataBase: new URL(getServerUrl()),
-	title: {
-		template: `%s - ${SITE_TITLE}`,
-		default: SITE_TITLE,
-	},
-	description: SITE_DESCRIPTION,
-	creator: "Angel Pichardo",
-	verification: {
-		google: "wgrEcrtUWVglsvPl05UfwC8pqkk8KfP-uQellDbz-gs",
-	},
-	category: "gaming",
-	openGraph: {
-		...GLOBAL_OG_PROPS.openGraph,
-		title: {
-			template: `%s - ${SITE_TITLE}`,
-			default: SITE_TITLE,
-		},
-		description: SITE_DESCRIPTION,
-		url: "/",
-	},
-	twitter: {
-		title: {
-			template: `%s - ${SITE_TITLE}`,
-			default: SITE_TITLE,
-		},
-		description: SITE_DESCRIPTION,
-		card: "summary_large_image",
-	},
-}
+  metadataBase: new URL(getServerUrl()),
+  title: {
+    template: `%s - ${SITE_TITLE}`,
+    default: SITE_TITLE,
+  },
+  description: SITE_DESCRIPTION,
+  creator: "Angel Pichardo",
+  verification: {
+    google: "wgrEcrtUWVglsvPl05UfwC8pqkk8KfP-uQellDbz-gs",
+  },
+  category: "gaming",
+  openGraph: {
+    ...GLOBAL_OG_PROPS.openGraph,
+    title: {
+      template: `%s - ${SITE_TITLE}`,
+      default: SITE_TITLE,
+    },
+    description: SITE_DESCRIPTION,
+    url: "/",
+  },
+  twitter: {
+    title: {
+      template: `%s - ${SITE_TITLE}`,
+      default: SITE_TITLE,
+    },
+    description: SITE_DESCRIPTION,
+    card: "summary_large_image",
+  },
+};
 
 export const viewport: Viewport = {
-	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "#ffffff" },
-		{ media: "(prefers-color-scheme: dark)", color: "#09090b" },
-	],
-}
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#09090b" },
+  ],
+};
 
 const geist = Geist({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-geist",
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-geist",
+});
 
 const geistMono = Geist_Mono({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-geist-mono",
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-geist-mono",
+});
 
 export default function RootLayout({ children }: LayoutProps) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			{IN_DEVELOPMENT && <ReactScanWrapper />}
-			<body
-				className={`${geist.className} ${geist.variable} ${geistMono.variable} flex min-h-dvh flex-col [&::-webkit-scrollbar-thumb:hover]:bg-neutral-500 dark:[&::-webkit-scrollbar-thumb:hover]:bg-neutral-600 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-neutral-400 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-700 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-2 `}
-			>
-				<KeyboardShortcutsProvider>
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-						disableTransitionOnChange
-					>
-						<Header />
-						{children}
-						<Toaster richColors position="top-center" closeButton />
-					</ThemeProvider>
-					<Analytics debug={false} />
-					<HashLinkHandler />
-					<GoogleAnalytics gaId="G-2M6PMT6Z3R" />
-				</KeyboardShortcutsProvider>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en" suppressHydrationWarning>
+      {IN_DEVELOPMENT && <ReactScanWrapper />}
+      <body
+        className={`${geist.className} ${geist.variable} ${geistMono.variable} flex min-h-dvh flex-col [&::-webkit-scrollbar-thumb:hover]:bg-neutral-500 dark:[&::-webkit-scrollbar-thumb:hover]:bg-neutral-600 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-neutral-400 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-700 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:w-2 `}
+      >
+        <KeyboardShortcutsProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+            <Toaster richColors position="top-center" closeButton />
+          </ThemeProvider>
+          <Analytics />
+          <SpeedInsights />
+          <HashLinkHandler />
+          <GoogleAnalytics gaId="G-2M6PMT6Z3R" />
+        </KeyboardShortcutsProvider>
+      </body>
+    </html>
+  );
 }
