6d023508a0bdaf31f47c93f0981b22be91f33b28
Angel Pichardo <pichardoangel416@gmail.com>
2026-03-15 14:49:16 -0400

    feat(relics): add Relic Effect to the main page

--- components/server/rich-blockquote.tsx
+++ components/server/rich-blockquote.tsx
@@ -1,12 +1,19 @@
 import { Info } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 interface BlockquoteProps {
 	children: React.ReactNode
+	className?: string
 }
 
-export function RichBlockquote({ children }: BlockquoteProps) {
+export function RichBlockquote({ children, className }: BlockquoteProps) {
 	return (
-		<blockquote className="mt-6 rounded-lg border border-orange-200 bg-orange-100/20 p-4 shadow-lg dark:border-orange-800 dark:bg-orange-900/20">
+		<blockquote
+			className={cn(
+				"mt-6 rounded-lg border border-orange-200 bg-orange-100/20 p-4 shadow-lg dark:border-orange-800 dark:bg-orange-900/20",
+				className,
+			)}
+		>
 			<div className="flex items-start text-orange-700 dark:text-orange-300">
 				<Info className="mr-2 h-5 w-5 shrink-0 text-orange-800 dark:text-orange-400" />
 				<em>{children}</em>
