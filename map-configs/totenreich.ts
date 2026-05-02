a55db9684d6cb1c2d638434e9d68c9406c4ff3e0
Angel Pichardo <pichardoangel416@gmail.com>
2026-05-02 14:57:05 -0400

    begin adding markers

--- map-configs/totenreich.ts
+++ map-configs/totenreich.ts
@@ -1,4 +1,5 @@
 import type { MapConfig } from "@/data/interactive-map"
+import { Option } from "effect"
 
 export const config: MapConfig = {
 	layers: [
@@ -6,7 +7,21 @@ export const config: MapConfig = {
 			id: "eidskallen",
 			title: "Eidskallen",
 			image: "/layers/totenreich/totenreich-eidskallen-layer.webp",
-			markers: [],
+			markers: [
+				{
+					id: "fishing-spot",
+					title: "Fishing Spot",
+					icon: Option.some("/icons/objectives/fish.webp"),
+					category: "objectives",
+					description:
+						"Location where you can fish for items to get rewards, and potentially get a green fish spawn for the main quest.",
+					locations: [
+						{ x: 0.529, y: 0.838 },
+						{ x: 0.621, y: 0.75 },
+						{ x: 0.71, y: 0.771 },
+					],
+				},
+			],
 		},
 		{
 			id: "boss-fight-arena",
